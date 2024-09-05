b8c0674afb912946c017357b53dc94725cb48fe7
GoldenPig1205 <pkj7354@gmail.com>
2024-09-05 14:40:23 +0900

    간단한 모드들은 즉시 추가

---
+++ RGM/Modes/KoreanSpeed.cs
@@ -0,0 +1,50 @@
+﻿using Exiled.API.Features.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Enums;
+using MEC;
+
+namespace RGM.Modes
+{
+    public class KoreaSpeed
+    {
+        public static KoreaSpeed Instance;
+
+        public void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Spawned += OnSpawned;
+            Exiled.Events.Handlers.Player.SearchingPickup += OnSearchingPickup;
+
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            foreach (var player in Player.List)
+            {
+                Spawned(player);
+            }
+
+            yield break;
+        }
+
+        public void OnSpawned(Exiled.Events.EventArgs.Player.SpawnedEventArgs ev)
+        {
+            Spawned(ev.Player);
+        }
+
+        public void Spawned(Player player)
+        {
+            player.EnableEffect(EffectType.MovementBoost, 125);
+        }
+
+        public void OnSearchingPickup(Exiled.Events.EventArgs.Player.SearchingPickupEventArgs ev)
+        {
+            ev.SearchTime = ev.SearchTime / 10;
+        }
+    }
+}
