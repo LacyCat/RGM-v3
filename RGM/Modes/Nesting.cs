c4014077ccb28f92177ce368cd8f74e15635f322
박경현 <pkj7354@gmail.com>
2024-09-05 13:41:10 +0900

    15개 모드 재작업 완료

---
+++ RGM/Modes/Nesting.cs
@@ -0,0 +1,34 @@
+﻿using Exiled.API.Features.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using MEC;
+
+namespace RGM.Modes
+{
+    public class Nesting
+    {
+        public static Nesting Instance;
+
+        public void OnEnabled()
+        {
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            while (true)
+            {
+                foreach (var player in Player.List.Where(x => x.IsAlive).ToList())
+                {
+                    player.MaxHealth = 50000;
+                }
+
+                yield return Timing.WaitForSeconds(1);
+            }
+        }
+    }
+}
