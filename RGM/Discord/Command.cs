e46daf9723ac7dc0df72543c1aa7ff8ed3f963a4
GoldenPig1205 <pkj7354@gmail.com>
2024-09-07 12:49:15 +0900

    디스코드에서 명령어 보내기

---
+++ RGM/Discord/Command.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using Exiled.API.Features;
+using System.Linq;
+
+namespace RGM.Discord
+{
+    class Command
+    {
+        public void OnEnabled()
+        {
+            Task.WhenAll(
+                Main()
+            );
+        }
+
+        static async Task Main()
+        {
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://127.0.0.1:9000/");
+            listener.Start();
+            Log.Info("Listening on http://127.0.0.1:9000/");
+
+            while (true)
+            {
+                var context = await listener.GetContextAsync();
+                _ = Task.Run(() => HandleRequest(context));
+            }
+        }
+
+        public static async void Request(Dictionary<string, object> dictionaryData, HttpListenerResponse response)
+        { 
+            string request = dictionaryData["data"].ToString();
+            List<string> strings = request.Split('/').ToList();
+            string Content = string.Join("/", strings);
+            string result = "null";
+
+            if (Content.StartsWith("print/"))
+            {
+                result = string.Join("/", strings);
+            }
+            else if (Content.StartsWith($"command/"))
+            {
+                result = Server.ExecuteCommand(strings[1]);
+            }
+
+            response.ContentLength64 = result.Length;
+            response.ContentType = "application/json";
+            await response.OutputStream.WriteAsync(Encoding.UTF8.GetBytes(result), 0, result.Length);
+
+        }
+
+        private static async Task HandleRequest(HttpListenerContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            if (request.HttpMethod == "POST")
+            {
+                using (var reader = new StreamReader(request.InputStream, request.ContentEncoding))
+                {
+                    var requestBody = await reader.ReadToEndAsync();
+
+                    var jsonData = SimpleJsonSerializer(new { data = requestBody });
+                    var dictionaryData = DeserializeJson(jsonData);
+
+                    Request(dictionaryData, response);
+                }
+            }
+            else if (request.HttpMethod == "GET")
+            {
+                var responseString = "<html><body><h1>Server is running</h1></body></html>";
+                var buffer = Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                response.ContentType = "text/html";
+                await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+            }
+            else
+            {
+                response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+            }
+
+            response.Close();
+        }
+
+        private static string SimpleJsonSerializer(object obj)
+        {
+            if (obj == null) return "null";
+
+            var type = obj.GetType();
+            if (type == typeof(string))
+            {
+                return $"\"{obj}\"";
+            }
+            else if (type.IsPrimitive)
+            {
+                return obj.ToString();
+            }
+            else if (type.IsArray)
+            {
+                var array = (Array)obj;
+                var elements = new List<string>();
+                foreach (var element in array)
+                {
+                    elements.Add(SimpleJsonSerializer(element));
+                }
+                return $"[{string.Join(",", elements)}]";
+            }
+            else
+            {
+                var properties = type.GetProperties();
+                var jsonElements = new List<string>();
+                foreach (var property in properties)
+                {
+                    var value = property.GetValue(obj);
+                    jsonElements.Add($"\"{property.Name}\":{SimpleJsonSerializer(value)}");
+                }
+                return $"{{{string.Join(",", jsonElements)}}}";
+            }
+        }
+
+        private static Dictionary<string, object> DeserializeJson(string jsonData)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonData);
+        }
+    }
+}
