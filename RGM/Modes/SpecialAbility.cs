1c81ba236094cce39a331684622bbaaaf81be3fc
박경현 <pkj7354@gmail.com>
2024-09-09 13:39:47 +0900

    특수 능력(처음부터 재작성 예정) 초안 작성됨

---
+++ RGM/Modes/SpecialAbility.cs
@@ -0,0 +1,26 @@
+﻿using Exiled.API.Features.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using MEC;
+
+namespace RGM.Modes
+{
+    public class SpecialAbility
+    {
+        public static SpecialAbility Instance;
+
+        public void OnEnabled()
+        {
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            yield break;
+        }
+    }
+}
