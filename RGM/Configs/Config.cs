597d2284105627ae812ee58df30d58f4686472dc
GoldenPig1205 <pkj7354@gmail.com>
2024-09-09 22:40:35 +0900

    test

--- RGM/Configs/Config.cs
+++ RGM/Configs/Config.cs
@@ -10,7 +10,7 @@ namespace RGM
 {
     public class Config : IConfig
     {
-        public bool IsEnabled { get; set; } = false;
+        public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
 
         public string WebhookURL { get; set; } = "https://discord.com/api/webhooks/1281967055272153099/b4-Qw3t5V4Tliq6axCheK0Ekv7_XdZ25MhbVMbepF_7WPz8RwsXw_c-3S58Cx3c8hj24";
