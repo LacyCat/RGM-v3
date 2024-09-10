660ef6c1c7c121fc45c0a1c5b531b9fedb5f6d3d
GoldenPig1205 <pkj7354@gmail.com>
2024-09-10 23:40:06 +0900

    많은 패치 (이스터에그 추가)

---
+++ RGM/Modes/RocketLauncher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using MEC;
+
+namespace RGM.Modes
+{
+    class RocketLauncher
+    {
+        public static RocketLauncher Instance;
+
+        public void OnEnabled()
+        {
+            Timing.RunCoroutine(OnModeStarted());
+
+            Exiled.Events.Handlers.Player.Hurt += OnHurt;
+        }
+
+        public void OnHurt(Exiled.Events.EventArgs.Player.HurtEventArgs ev)
+        {
+            if (!ev.DamageHandler.IsFriendlyFire)
+                Server.ExecuteCommand($"/rocket {ev.Player.Id} 0.3");
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            yield return 0f;
+        }
+    }
+}
