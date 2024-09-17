1d1115badab97aa80cd269513ea1fb0e04709755
GoldenPig1205 <pkj7354@gmail.com>
2024-09-18 00:19:23 +0900

    축복ㅣ고치려고 노력함

--- RGM/Modes/Blessing.cs
+++ RGM/Modes/Blessing.cs
@@ -29,27 +29,34 @@ namespace RGM.Modes
             {
                 foreach (var player in Player.List)
                 {
-                    if (player.IsAlive)
+                    try
                     {
-                        int s = player.CurrentSpectatingPlayers.Count();
-                        player.ShowHint($"현재 {s}명이 당신을 관전하고 있습니다.", 1.2f);
-
-                        player.GetEffect(EffectType.MovementBoost).Intensity = (byte)(5 * s);
-                        player.GetEffect(EffectType.DamageReduction).Intensity = (byte)(2 * s);
-                        player.Heal(0.35f * s);
-                        player.Scale = new Vector3(1 - s * 0.02f, 1 - s * 0.02f, 1 - s * 0.02f);
-                    }
-                    else
-                    {
-                        if (player.Role is SpectatorRole spectator)
+                        if (player.IsAlive)
                         {
-                            int s = spectator.SpectatedPlayer.CurrentSpectatingPlayers.Count();
-                            player.ShowHint($"현재 {s}명이 이 플레이어를 관전하고 있습니다.", 1.2f);
+                            int s = player.CurrentSpectatingPlayers.Count();
+                            player.ShowHint($"현재 {s}명이 당신을 관전하고 있습니다.", 1.2f);
+
+                            player.GetEffect(EffectType.MovementBoost).Intensity = (byte)(5 * s);
+                            player.GetEffect(EffectType.DamageReduction).Intensity = (byte)(2 * s);
+                            player.Heal(0.35f * s);
+                            player.Scale = new Vector3(1 - s * 0.02f, 1 - s * 0.02f, 1 - s * 0.02f);
                         }
+                        else
+                        {
+                            if (player.Role is SpectatorRole spectator)
+                            {
+                                int s = spectator.SpectatedPlayer.CurrentSpectatingPlayers.Count();
+                                player.ShowHint($"현재 {s}명이 이 플레이어를 관전하고 있습니다.", 1.2f);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error($"{e}");
                     }
                 }
 
-                yield return Timing.WaitForSeconds(1);
+                yield return Timing.WaitForSeconds(1f);
             }
         }
 
