73e4722b0399923aadf35d02cc2095ff8e9edc5b
GoldenPig1205 <pkj7354@gmail.com>
2024-09-17 23:25:46 +0900

    빨간 불, 초록 불ㅣ추가

---
+++ RGM/Modes/RedLightGreenLight.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using MEC;
+using Mirror;
+using UnityEngine;
+
+namespace RGM.Modes
+{
+    class RedLightGreenLight
+    {
+        public static RedLightGreenLight Instance;
+
+        public string Light = "Green";
+        public Dictionary<Player, Vector3> PlayerPosition = new Dictionary<Player, Vector3>();
+        public Dictionary<Player, Quaternion> PlayerRotation = new Dictionary<Player, Quaternion>();
+
+        public void OnEnabled()
+        {
+            Timing.RunCoroutine(OnModeStarted());
+            Timing.RunCoroutine(RecordPlayerInfo());
+            Timing.RunCoroutine(CheckRedLight());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            yield return Timing.WaitForSeconds(10f);
+
+            Player.List.ToList().ForEach(x => x.ShowHint($"<color=green>초록 불</color>! 움직여도 됩니다.", 250));
+
+            while (true)
+            {
+                yield return Timing.WaitForSeconds(UnityEngine.Random.Range(1, 50));
+
+                Light = "Red";
+                Player.List.ToList().ForEach(x => x.ShowHint($"<color=red>빨간 불</color>! 움직이지 마세요!", 250));
+
+                yield return Timing.WaitForSeconds(UnityEngine.Random.Range(1, 5));
+
+                Light = "Green";
+                Player.List.ToList().ForEach(x => x.ShowHint($"<color=green>초록 불</color>! 움직여도 됩니다.", 250));
+            }
+        }
+
+        public IEnumerator<float> RecordPlayerInfo()
+        {
+            yield return Timing.WaitForSeconds(10f);
+
+            while (true)
+            {
+                foreach (var player in Player.List)
+                {
+                    if (PlayerPosition.ContainsKey(player))
+                    {
+                        PlayerPosition[player] = player.Position;
+                        PlayerRotation[player] = player.Rotation;
+                    }
+                    else
+                    {
+                        PlayerPosition.Add(player, player.Position);
+                        PlayerRotation.Add(player, player.Rotation);
+                    }
+
+                    yield return Timing.WaitForSeconds(0.1f);
+                }
+            }
+        }
+
+        public IEnumerator<float> CheckRedLight()
+        {
+            yield return Timing.WaitForSeconds(10f);
+
+            while (true)
+            {
+                while (Light == "Green")
+                    yield return Timing.WaitForSeconds(0.1f);
+
+                yield return Timing.WaitForSeconds(1.5f);
+
+                while (Light == "Red")
+                {
+                    foreach (var player in Player.List)
+                    {
+                        if (PlayerPosition.ContainsKey(player) && player.IsAlive)
+                        {
+                            if (PlayerPosition[player] != player.Position || PlayerRotation[player] != player.Rotation)
+                            {
+                                Server.ExecuteCommand($"/rocket {player.Id} 0.01");
+
+                                Server.ExecuteCommand($"/clearcassie");
+                                Server.ExecuteCommand($"/cassie_sl 안타깝게도 {player.DisplayNickname}(이)가 <b><color=red>빨간 불</color></b>에 건넜습니다.");
+                            }
+                        }
+                    }
+
+                    yield return Timing.WaitForSeconds(0.2f);
+                }
+            }
+        }
+    }
+}
