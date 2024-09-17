a7e969bf0539a666102e9179140272e8ceea7139
GoldenPig1205 <pkj7354@gmail.com>
2024-09-18 00:33:03 +0900

    버그들을 수정하려고 노력함

--- RGM/Modes/Blessing.cs
+++ RGM/Modes/Blessing.cs
@@ -29,30 +29,23 @@ namespace RGM.Modes
             {
                 foreach (var player in Player.List)
                 {
-                    try
+                    if (player.IsAlive)
                     {
-                        if (player.IsAlive)
-                        {
-                            int s = player.CurrentSpectatingPlayers.Count();
-                            player.ShowHint($"현재 {s}명이 당신을 관전하고 있습니다.", 1.2f);
+                        int s = player.CurrentSpectatingPlayers.Count();
+                        player.ShowHint($"현재 {s}명이 당신을 관전하고 있습니다.", 1.2f);
 
-                            player.GetEffect(EffectType.MovementBoost).Intensity = (byte)(5 * s);
-                            player.GetEffect(EffectType.DamageReduction).Intensity = (byte)(2 * s);
-                            player.Heal(0.35f * s);
-                            player.Scale = new Vector3(1 - s * 0.02f, 1 - s * 0.02f, 1 - s * 0.02f);
-                        }
-                        else
-                        {
-                            if (player.Role is SpectatorRole spectator)
-                            {
-                                int s = spectator.SpectatedPlayer.CurrentSpectatingPlayers.Count();
-                                player.ShowHint($"현재 {s}명이 이 플레이어를 관전하고 있습니다.", 1.2f);
-                            }
-                        }
+                        player.EnableEffect(EffectType.MovementBoost, 5 * s);
+                        player.EnableEffect(EffectType.DamageReduction, 2 * s);
+                        player.Heal(0.35f * s);
+                        player.Scale = new Vector3(1 - s * 0.02f, 1 - s * 0.02f, 1 - s * 0.02f);
                     }
-                    catch (Exception e)
+                    else
                     {
-                        Log.Error($"{e}");
+                        if (player.Role is SpectatorRole spectator)
+                        {
+                            int s = spectator.SpectatedPlayer.CurrentSpectatingPlayers.Count();
+                            player.ShowHint($"현재 {s}명이 이 플레이어를 관전하고 있습니다.", 1.2f);
+                        }
                     }
                 }
 
