2788801159355048ff46c9e16665af50dee5a3fb
박경현 <pkj7354@gmail.com>
2024-09-19 13:31:07 +0900

    스플리프ㅣ렉을 개선하기 위한 패치

--- RGM/Modes/Spleef.cs
+++ RGM/Modes/Spleef.cs
@@ -15,6 +15,7 @@ using Mirror;
 using MultiBroadcast;
 using site02;
 using UnityEngine;
+using Exiled.API.Enums;
 
 namespace RGM.Modes
 {
@@ -52,45 +53,52 @@ namespace RGM.Modes
                 player.Position = new Vector3(41.58984f, 1044.594f, -113.3477f);
             }
 
-            void Processing(GameObject gameObject)
+            IEnumerator<float> Processing(GameObject gameObject)
             {
                 Primitive Platform = gameObject.GetComponent<PrimitiveObject>().Primitive;
                 Platform.Color = new Color(255, 0, 0);
 
-                Timing.CallDelayed(0.75f, Platform.Destroy);
+                yield return Timing.WaitForSeconds(0.75f);
+
+                Platform.Destroy();
+            }
+
+            IEnumerator<float> PlatformCheck(Player player)
+            {
+                if (Physics.Raycast(player.Position, Vector3.down, out RaycastHit hit, 1f, (LayerMask)1))
+                {
+                    if (hit.transform.name == "Platform")
+                        Timing.RunCoroutine(Processing(hit.transform.gameObject));
+
+                    else if (hit.transform.name == "Lava")
+                        player.Kill("용암을 좋아한 나머지 뛰어들어갔습니다.");
+                }
+                else
+                {
+                    Transform closestPlatform = null;
+                    float closestDistance = float.MaxValue;
+
+                    foreach (var platform in GameObject.FindObjectsOfType<Transform>().Where(t => t.name == "Platform").ToList())
+                    {
+                        float distance = Vector3.Distance(player.Position, platform.transform.position);
+
+                        if (distance < closestDistance)
+                        {
+                            closestPlatform = platform.transform;
+                            closestDistance = distance;
+                        }
+                    }
+
+                    Timing.RunCoroutine(Processing(closestPlatform.gameObject));
+                }
+
+                yield return 1f;
             }
 
             while (true)
             {
                 foreach (var player in Player.List.Where(x => x.IsAlive).ToList())
-                {
-                    if (Physics.Raycast(player.Position, Vector3.down, out RaycastHit hit, 1f, (LayerMask)1))
-                    {
-                        if (hit.transform.name == "Platform")
-                            Processing(hit.transform.gameObject);
-
-                        else if (hit.transform.name == "Lava")
-                            player.Kill("용암을 좋아한 나머지 뛰어들어갔습니다.");
-                    }
-                    else
-                    {
-                        Transform closestPlatform = null;
-                        float closestDistance = float.MaxValue;
-
-                        foreach (var platform in GameObject.FindObjectsOfType<Transform>().Where(t => t.name == "Platform").ToList())
-                        {
-                            float distance = Vector3.Distance(player.Position, platform.transform.position);
-
-                            if (distance < closestDistance)
-                            {
-                                closestPlatform = platform.transform;
-                                closestDistance = distance;
-                            }
-                        }
-
-                        Processing(closestPlatform.gameObject);
-                    }
-                }
+                    Timing.RunCoroutine(PlatformCheck(player));
 
                 yield return Timing.WaitForSeconds(0.1f);
             }
@@ -109,7 +117,7 @@ namespace RGM.Modes
 
         public void OnHurting(Exiled.Events.EventArgs.Player.HurtingEventArgs ev)
         {
-            if (ev.DamageHandler.Type == Exiled.API.Enums.DamageType.Falldown)
+            if (ev.DamageHandler.Type == DamageType.Falldown)
             {
                 ev.IsAllowed = false;
             }
