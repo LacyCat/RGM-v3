57ea8f47e94545ee7d4701a0b2f1d7237bc2b757
GoldenPig1205 <pkj7354@gmail.com>
2024-09-20 23:38:47 +0900

    빨간 불, 초록 불ㅣ체크 딜레이

--- RGM/Configs/Modes.cs
+++ RGM/Configs/Modes.cs
@@ -57,7 +57,11 @@ namespace RGM
             { "시베리아", new List<string>() { "FAFAFA", "최대한 다른 자들과 붙어 온기를 나눠 가지세요!", "Siberia", "public", "몬키키", "" } },
             { "소울메이트", new List<string>() { "FF00FF", "단짝 친구와 모든 것을 함께하세요!", "SoulMate", "public", "", "" } },
             { "특수능력", new List<string>() { "2EFEF7", "모두가 특수 능력을 지니고 시작합니다.", "SpecialAbility", "private", "", "" } },
-            { "스피릿", new List<string>() { "CED8F6", "죽으면 영혼 상태에 돌입합니다!", "Spirit", "public", "", "" } },
+            { "스피릿", new List<string>() { "CED8F6", "죽으면 영혼 상태에 돌입합니다!", "Spirit", "public", "", 
+"""
+죽으면 영혼 상태로 부활합니다. 이 상태에서 사망하면 성불됩니다.
+또한, 자살로 사망한 경우 곧바로 성불됩니다.
+""" } },
             { "스플리프", new List<string>() { "BEF781", "떨어지지 않으려면 계속 움직이세요!", "Spleef", "public", "", "" } },
             { "기본템", new List<string>() { "FA5858", "5~8개의 아이템이 사전에 지급됩니다.\n기존에 가지고 있던 아이템은 제거됩니다.", "StandardItems", "private", "", "" } },
             { "슈퍼 스타", new List<string>() { "FE2EF7", "모두의 마이크가 공유됩니다.", "SuperStar", "public", "", "" } },
