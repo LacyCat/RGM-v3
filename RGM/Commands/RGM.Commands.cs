eaf20683c008c7b7627b19d697cf8d5769c95b17
GoldenPig1205 <pkj7354@gmail.com>
2024-09-21 02:03:46 +0900

    .자살 명령어가 추가되었습니다.

--- RGM/Commands/RGM.Commands.cs
+++ RGM/Commands/RGM.Commands.cs
@@ -10,6 +10,36 @@ using UnityEngine;
 namespace RGM.Commands
 {
     [CommandHandler(typeof(ClientCommandHandler))]
+    public class Suicide : ICommand
+    {
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(sender);
+
+            if (player.IsAlive && Round.IsStarted)
+            {
+                player.IsGodModeEnabled = false;
+                player.Kill("벌레를 피하다가 사망하였습니다.");
+                response = "당신의 기도는 저 하늘에 닿았습니다.";
+                return true;
+            }
+            else
+            {
+                response = "이미 하늘나라에 있는 상태입니다.";
+                return false;
+            }
+        }
+
+        public string Command { get; } = "suicide";
+
+        public string[] Aliases { get; } = { "자살", "wktkf" };
+
+        public string Description { get; } = "[RGM] 스스로 생을 마감할 수 있습니다.";
+
+        public bool SanitizeResponse { get; } = true;
+    }
+
+    [CommandHandler(typeof(ClientCommandHandler))]
     public class Report : ICommand
     {
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
