c324295c939750655e82f5bb97c05938fd7c7429
GoldenPig1205 <pkj7354@gmail.com>
2024-09-21 00:22:20 +0900

    스플리프ㅣ렉 걸리는 요소를 찾는 중..

--- RGM/Modes/Spleef.cs
+++ RGM/Modes/Spleef.cs
@@ -53,52 +53,47 @@ namespace RGM.Modes
                 player.Position = new Vector3(41.58984f, 1044.594f, -113.3477f);
             }
 
-            IEnumerator<float> Processing(GameObject gameObject)
+            async void Processing(GameObject gameObject)
             {
                 Primitive Platform = gameObject.GetComponent<PrimitiveObject>().Primitive;
                 Platform.Color = new Color(255, 0, 0);
 
-                yield return Timing.WaitForSeconds(0.75f);
+                await Task.Delay(750);
 
                 Platform.Destroy();
-            }
-
-            IEnumerator<float> PlatformCheck(Player player)
-            {
-                if (Physics.Raycast(player.Position, Vector3.down, out RaycastHit hit, 1f, (LayerMask)1))
-                {
-                    if (hit.transform.name == "Platform")
-                        Timing.RunCoroutine(Processing(hit.transform.gameObject));
-
-                    else if (hit.transform.name == "Lava")
-                        player.Kill("용암을 좋아한 나머지 뛰어들어갔습니다.");
-                }
-                else
-                {
-                    Transform closestPlatform = null;
-                    float closestDistance = float.MaxValue;
-
-                    foreach (var platform in GameObject.FindObjectsOfType<Transform>().Where(t => t.name == "Platform").ToList())
-                    {
-                        float distance = Vector3.Distance(player.Position, platform.transform.position);
-
-                        if (distance < closestDistance)
-                        {
-                            closestPlatform = platform.transform;
-                            closestDistance = distance;
-                        }
-                    }
-
-                    Timing.RunCoroutine(Processing(closestPlatform.gameObject));
-                }
-
-                yield return 1f;
             }
 
             while (true)
             {
                 foreach (var player in Player.List.Where(x => x.IsAlive).ToList())
-                    Timing.RunCoroutine(PlatformCheck(player));
+                {
+                    if (Physics.Raycast(player.Position, Vector3.down, out RaycastHit hit, 1f, (LayerMask)1))
+                    {
+                        if (hit.transform.name == "Platform")
+                            Processing(hit.transform.gameObject);
+
+                        else if (hit.transform.name == "Lava")
+                            player.Kill("용암을 좋아한 나머지 뛰어들어갔습니다.");
+                    }
+                    else
+                    {
+                        Transform closestPlatform = null;
+                        float closestDistance = float.MaxValue;
+
+                        foreach (var platform in GameObject.FindObjectsOfType<Transform>().Where(t => t.name == "Platform").ToList())
+                        {
+                            float distance = Vector3.Distance(player.Position, platform.transform.position);
+
+                            if (distance < closestDistance)
+                            {
+                                closestPlatform = platform.transform;
+                                closestDistance = distance;
+                            }
+                        }
+
+                        Processing(closestPlatform.gameObject);
+                    }
+                }
 
                 yield return Timing.WaitForSeconds(0.1f);
             }
