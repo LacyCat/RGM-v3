2424b50937fc119fe729edc0e2e8f1b836208e95
GoldenPig1205 <pkj7354@gmail.com>
2024-09-21 16:45:49 +0900

    데스런ㅣ게임 시작 후 5초 간 유예 시간

--- RGM/Modes/DeathRun.cs
+++ RGM/Modes/DeathRun.cs
@@ -10,6 +10,7 @@ using UnityEngine;
 using Exiled.API.Features.Roles;
 using Exiled.API.Enums;
 using PlayerRoles;
+using MultiBroadcast.API;
 
 namespace RGM.Modes
 {
@@ -33,13 +34,23 @@ namespace RGM.Modes
             Tagger = RGM.GetRandomValue(Player.List.ToList());
 
             Tagger.Role.Set(RoleTypeId.Scientist);
-            Tagger.Position = new Vector3(48.86719f, 999.6483f, 86.34375f);
+            Tagger.Position = new Vector3(44.57422f, 999.7067f, 78.52734f);
             Tagger.ClearInventory();
+
+            for (int i=1; i<6; i++)
+            {
+                foreach (var player in Player.List)
+                {
+                    player.ClearPlayerBroadcasts();
+                    player.AddBroadcast(2, $"<size=30><b><color=red>{6 - i}</color>초 후 게임이 시작됩니다. 준비하세요!</size>");
+                }
+                yield return Timing.WaitForSeconds(1f);
+            }
 
             foreach (var player in Player.List.Where(x => x != Tagger))
             {
                 player.Role.Set(RoleTypeId.ClassD);
-                player.Position = new Vector3(44.57422f, 999.7067f, 78.52734f);
+                player.Position = new Vector3(48.86719f, 999.6483f, 86.34375f);
             }
         }
     }
