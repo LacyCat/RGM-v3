f671acc2850cf3016e3cf58cab755aa44cf9ab54
GoldenPig1205 <pkj7354@gmail.com>
2024-09-23 22:45:54 +0900

    중도 참여에도 모드 설명 보이도록 수정

--- RGM/Configs/Notions.cs
+++ RGM/Configs/Notions.cs
@@ -9,7 +9,7 @@ namespace RGM
     public class Notions
     {
         public static string StartModeDescription { get; set; } = "<size=30><b><color=#{ModeColor}>{CurrentMode}</color></b></size>\n<size=25>{ModeDescription}</size>\n<size=15>콘솔(~ 또는 `)을 열고 자세한 설명을 확인해보세요.</size>";
-        public static string LateJoinModeDescription { get; set; } = "<size=20>현재 진행중인 모드</size>\n<size=25><b><color=#{ModeColor}>{CurrentMode}</color></b></size>\n<size=15>콘솔(~ 또는 `)을 열고 자세한 설명을 확인해보세요.</size>";
+        public static string LateJoinModeDescription { get; set; } = "<size=20>현재 진행중인 모드</size>\n<size=25><b><color=#{ModeColor}>{CurrentMode}</color></b></size>\n<size=20>{ModeDescription}</size>\n<size=15>콘솔(~ 또는 `)을 열고 자세한 설명을 확인해보세요.</size>";
         public static string WelcomeMessage { get; set; } = "<size=25><b>랜덤게임모드</b>에 오신 것을 환영합니다!</size>";
         public static string LobbyMessage { get; set; } = "\n<align=left>\n{FirstMark} [1] {First} | {FirstVote}\n{SecondMark} [2] {Second} | {SecondVote}\n{ThirdMark} [3] {Third} | {ThirdVote}\n</align>\n\n<align=left><color=#{ModeColor}><b>{ModeName}</b></color>\n{ModeDescription}</align>{Lines}\n\n\n\n\n";
     }
