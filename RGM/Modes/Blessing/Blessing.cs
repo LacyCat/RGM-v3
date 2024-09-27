665988a7182cf74f1dc9a4e7811decfa546991b8
GoldenPig1205 <pkj7354@gmail.com>
2024-09-27 19:16:51 +0900

    축복ㅣ완전히 모드를 수정하였습니다.

--- RGM/Modes/Blessing/Blessing.cs
+++ RGM/Modes/Blessing/Blessing.cs
@@ -34,8 +34,8 @@ namespace RGM.Modes
                         int s = player.CurrentSpectatingPlayers.Count();
                         player.ShowHint($"현재 {s}명이 당신을 관전하고 있습니다.", 1.2f);
 
-                        player.EnableEffect(EffectType.MovementBoost, 5 * s);
-                        player.EnableEffect(EffectType.DamageReduction, 2 * s);
+                        player.GetEffect(EffectType.MovementBoost).Intensity = (byte)(5 * s);
+                        player.GetEffect(EffectType.DamageReduction).Intensity = (byte)(5 * s);
                         player.Heal(0.35f * s);
                     }
                     else
