b987ff0f0d699377917dbda27ebb6a7e4b53ae26
GoldenPig1205 <pkj7354@gmail.com>
2024-10-08 23:36:30 +0900

    꼬리 잡기ㅣ첫 릴리즈

---
+++ RGM/Modes/TailCatcher/TailCatcher.cs
@@ -0,0 +1,169 @@
+﻿using Exiled.API.Features.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using MEC;
+using UnityEngine;
+using Exiled.API.Features.Roles;
+using Exiled.API.Enums;
+using PlayerRoles;
+using MultiBroadcast.API;
+using RGM.API;
+using Mirror;
+
+namespace RGM.Modes
+{
+    public class TailCatcher
+    {
+        public static TailCatcher Instance;
+
+        public List<Player> pl = new List<Player>();
+
+        ReferenceHub dj;
+
+        public void OnEnabled()
+        {
+            Round.IsLocked = true;
+            Respawn.TimeUntilNextPhase = 10000;
+
+            Exiled.Events.Handlers.Player.DroppingItem += OnDroppingItem;
+            Exiled.Events.Handlers.Player.DroppingAmmo += OnDroppingAmmo;
+            Exiled.Events.Handlers.Player.Shot += OnShot;
+            Exiled.Events.Handlers.Player.Hurting += OnHurting;
+            Exiled.Events.Handlers.Player.Died += OnDied;
+
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            Server.ExecuteCommand($"/mp load hp");
+
+            Player.List.Where(x => !x.IsNPC).CopyTo(pl);
+
+            dj = GGUtils.Gtool.Spawn(RoleTypeId.Tutorial, new Vector3(82.51834f, 1014.692f, -50.10588f));
+
+            Dictionary<ReferenceHub, string> register = new Dictionary<ReferenceHub, string>()
+            {
+                { dj, "dj" }
+            };
+
+            foreach (var reg in register)
+            {
+                try
+                {
+                    GGUtils.Gtool.Register(reg.Key, reg.Value);
+                }
+                catch
+                {
+                }
+            }
+
+            GGUtils.Gtool.PlayerGet("dj").DisplayNickname = "DJ";
+            GGUtils.Gtool.PlaySound("dj", "Initial_D_Dancing", VoiceChat.VoiceChatChannel.Intercom, 25, true);
+
+            Timing.RunCoroutine(DJHeadBanging());
+
+            foreach (var player in Player.List.Where(x => !x.IsNPC))
+            {
+                player.Role.Set(RoleTypeId.ClassD);
+                player.Position = new Vector3(83.91287f, 1014.692f, -37.13322f);
+                player.AddItem(ItemType.GunCrossvec);
+            }
+
+            pl.ShuffleList();
+
+            while (true)
+            {
+                foreach (var player in Player.List.Where(pl.Contains))
+                {
+                    int playerIndex = pl.IndexOf(player);
+                    int targetIndex = (playerIndex + 1) % pl.Count;
+
+                    Player target = pl[targetIndex];
+
+                    player.ShowHint($"당신의 타깃 : {target.Nickname}", 1.2f);
+                }
+
+                yield return Timing.WaitForSeconds(1f);
+            }
+        }
+
+        public IEnumerator<float> DJHeadBanging()
+        {
+            yield return Timing.WaitForSeconds(1f);
+
+            bool HeadUp = true;
+
+            while (true)
+            {
+                if (HeadUp)
+                {
+                    GGUtils.Gtool.Rotate(dj, new Vector3(0, -1f, 0));
+
+                    HeadUp = false;
+
+                    yield return Timing.WaitForSeconds(0.2f);
+                }
+                else
+                {
+                    GGUtils.Gtool.Rotate(dj, new Vector3(0, 1f, 0));
+
+                    HeadUp = true;
+
+                    yield return Timing.WaitForSeconds(0.15f);
+                }
+            }
+        }
+
+        public void OnDroppingItem(Exiled.Events.EventArgs.Player.DroppingItemEventArgs ev)
+        {
+            ev.IsAllowed = false;
+        }
+
+        public void OnDroppingAmmo(Exiled.Events.EventArgs.Player.DroppingAmmoEventArgs ev)
+        {
+            ev.IsAllowed = false;
+        }
+
+        public void OnShot(Exiled.Events.EventArgs.Player.ShotEventArgs ev)
+        {
+            ev.Firearm.Ammo += 1;
+        }
+
+        public void OnHurting(Exiled.Events.EventArgs.Player.HurtingEventArgs ev)
+        {
+            int attackerIndex = pl.IndexOf(ev.Attacker);
+            int targetIndex = (attackerIndex + 1) % pl.Count;
+
+            if (targetIndex == 0 && attackerIndex == pl.Count - 1)
+            {
+                Player target = pl[0];
+            }
+            else if (targetIndex == attackerIndex + 1)
+            {
+                Player target = pl[targetIndex];
+            }
+            else
+            {
+                ev.IsAllowed = false;
+                ev.Attacker.Hurt(ev.Amount);
+            }
+        }
+
+
+        public void OnDied(Exiled.Events.EventArgs.Player.DiedEventArgs ev)
+        {
+            if (pl.Contains(ev.Player))
+            {
+                pl.Remove(ev.Player);
+
+                if (pl.Count < 2)
+                    Round.IsLocked = false;
+            }
+        }
+    }
+}
