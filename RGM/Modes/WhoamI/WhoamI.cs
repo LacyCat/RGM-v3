ec485cf77134ff8a531cbf1e3a8ce4dbe51cf86b
GoldenPig1205 <pkj7354@gmail.com>
2024-10-11 16:01:18 +0900

    사망 메세지 더 아름답게 수정

--- RGM/Modes/WhoamI/WhoamI.cs
+++ RGM/Modes/WhoamI/WhoamI.cs
@@ -12,6 +12,7 @@ using Achievements.Handlers;
 using Exiled.API.Enums;
 using UnityEngine;
 using CustomPlayerEffects;
+using MultiBroadcast.API;
 
 namespace RGM.Modes
 {
@@ -28,7 +29,7 @@ namespace RGM.Modes
         {
             while (true)
             {
-                yield return Timing.WaitForSeconds(60f);
+                Player.List.ToList().ForEach(x => x.AddBroadcast(1, "1구간 통과"));
 
                 List<RoleTypeId> BlackList = new List<RoleTypeId>()
                 {
@@ -37,13 +38,16 @@ namespace RGM.Modes
                     RoleTypeId.Overwatch,
                     RoleTypeId.Scp079
                 };
+
                 Dictionary<Player, List<object>> PlayersInfo = new Dictionary<Player, List<object>>();
+
+                Player.List.ToList().ForEach(x => x.AddBroadcast(1, "2구간 통과"));
 
                 foreach (var player in Player.List.Where(x => !BlackList.Contains(x.Role.Type)))
                 {
                     PlayersInfo.Add(player, new List<object>
                     {
-                        player.Role,
+                        player.Role.Type,
                         player.MaxHealth,
                         player.Health,
                         player.ActiveEffects,
@@ -54,35 +58,34 @@ namespace RGM.Modes
                     });
                 }
 
+                Player.List.ToList().ForEach(x => x.AddBroadcast(1, "3구간 통과"));
+
                 foreach (var player in Player.List.Where(PlayersInfo.ContainsKey))
                 {
-                    try
-                    {
-                        Player p = Tools.GetRandomValue(PlayersInfo.Keys.ToList());
+                    Player p = Tools.GetRandomValue(PlayersInfo.Keys.ToList());
 
-                        player.Role.Set((RoleTypeId)PlayersInfo[p][0]);
-                        player.MaxHealth = (int)PlayersInfo[p][1];
-                        player.Health = (int)PlayersInfo[p][2];
+                    player.Role.Set((RoleTypeId)PlayersInfo[p][0]);
+                    player.MaxHealth = (int)PlayersInfo[p][1];
+                    player.Health = (int)PlayersInfo[p][2];
 
-                        foreach (var effect in (List<StatusEffectBase>)PlayersInfo[p][3])
-                            player.EnableEffect(effect);
+                    foreach (var effect in (List<StatusEffectBase>)PlayersInfo[p][3])
+                        player.EnableEffect(effect);
 
-                        player.ClearInventory();
+                    player.ClearInventory();
 
-                        foreach (var item in (List<Item>)PlayersInfo[p][4])
-                            player.AddItem(item.Type);
+                    foreach (var item in (List<Item>)PlayersInfo[p][4])
+                        player.AddItem(item.Type);
 
-                        player.CurrentItem = player.Items.ToList().Find(x => x.Type == (ItemType)PlayersInfo[p][5]);
-                        player.Position = (Vector3)PlayersInfo[p][5];
-                        SLPlayerRotation.Extensions.SetHubRotation(player, (Quaternion)PlayersInfo[p][6]);
+                    player.CurrentItem = player.Items.ToList().Find(x => x.Type == (ItemType)PlayersInfo[p][5]);
+                    player.Position = (Vector3)PlayersInfo[p][5];
+                    SLPlayerRotation.Extensions.SetHubRotation(player, (Quaternion)PlayersInfo[p][6]);
 
-                        PlayersInfo.Remove(p);
-                    }
-                    catch (Exception e)
-                    {
-                        Log.Error($"{e}");
-                    }
+                    PlayersInfo.Remove(p);
                 }
+
+                Player.List.ToList().ForEach(x => x.AddBroadcast(1, "4구간 통과"));
+
+                yield return Timing.WaitForSeconds(60f);
             }
         }
     }
