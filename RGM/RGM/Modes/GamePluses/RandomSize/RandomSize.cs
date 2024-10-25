8b636d63d733b6870ae3853f6968f5f0f1766950
GoldenPig1205 <pkj7354@gmail.com>
2024-10-25 15:03:49 +0900

    랜덤크기ㅣ서브 모드로 추가

---
+++ RGM/RGM/Modes/GamePluses/RandomSize/RandomSize.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomRendering;
+using Exiled.API.Features;
+using MEC;
+using Mirror;
+using RGM.API.Features;
+using RGM.API.DataBases;
+using UnityEngine;
+using Exiled.API.Features.Items;
+
+namespace RGM.Modes
+{
+    class RandomSize
+    {
+        public static RandomSize Instance;
+
+        public void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Spawned += OnSpawned;
+
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            foreach (var player in Player.List)
+            {
+                Spawned(player);
+            }
+
+            yield break;
+        }
+
+        public void OnSpawned(Exiled.Events.EventArgs.Player.SpawnedEventArgs ev)
+        {
+            Spawned(ev.Player);
+        }
+
+        public void Spawned(Player player)
+        {
+            Timing.CallDelayed(1f, () => 
+            {
+                player.Scale = new Vector3(UnityEngine.Random.Range(0.1f, 1.2f), UnityEngine.Random.Range(0.3f, 1.2f), UnityEngine.Random.Range(0.1f, 1.2f));
+            });
+        }
+    }
+}
