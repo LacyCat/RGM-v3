34c7c1cf224291d0f631e769b6d62fe2e8cae6fb
GoldenPig1205 <pkj7354@gmail.com>
2024-10-26 22:00:45 +0900

    Spooky!ㅣpublic로 추가

---
+++ RGM/RGM/Modes/GamePluses/Spooky/Spooky.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomRendering;
+using Exiled.API.Features;
+using MEC;
+using Mirror;
+using RGM.API.Features;
+using RGM.API.DataBases;
+using UnityEngine;
+using Exiled.API.Features.Items;
+using Exiled.API.Enums;
+
+namespace RGM.Modes
+{
+    class Spooky
+    {
+        public static Spooky Instance;
+
+        public void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Spawned += OnSpawned;
+
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            foreach (var player in Player.List)
+            {
+                Spawned(player);
+            }
+
+            yield break;
+        }
+
+        public void OnSpawned(Exiled.Events.EventArgs.Player.SpawnedEventArgs ev)
+        {
+            Spawned(ev.Player);
+        }
+
+        public void Spawned(Player player)
+        {
+            player.EnableEffect(Tools.GetRandomValue(Datas.FunnyEffects));
+        }
+    }
+}
