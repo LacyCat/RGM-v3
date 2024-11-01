e26e5485a10c2b09e667b930d7c00549e242f3d8
GoldenPig1205 <pkj7354@gmail.com>
2024-11-01 23:43:59 +0900

    종이 인간ㅣ서브 모드로 첫 릴리즈

---
+++ RGM/RGM/Modes/GamePluses/PaperMan/PaperMan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomRendering;
+using Exiled.API.Features;
+using MEC;
+using Mirror;
+using RGM.API.Features;
+using RGM.API.DataBases;
+using UnityEngine;
+using Exiled.API.Features.Items;
+
+namespace RGM.Modes
+{
+    class PaperMan
+    {
+        public static PaperMan Instance;
+
+        public void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Spawned += OnSpawned;
+
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            foreach (var player in Player.List)
+            {
+                Spawned(player);
+            }
+
+            yield break;
+        }
+
+        public void OnSpawned(Exiled.Events.EventArgs.Player.SpawnedEventArgs ev)
+        {
+            Spawned(ev.Player);
+        }
+
+        public void Spawned(Player player)
+        {
+            Timing.CallDelayed(1f, () =>
+            {
+                player.Scale = new Vector3(0.01f, 1, 1f);
+            });
+        }
+    }
+}
