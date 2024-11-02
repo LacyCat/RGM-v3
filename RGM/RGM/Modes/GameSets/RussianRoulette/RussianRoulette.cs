81125ab115e7275d7d336706741ae5a9c5fcc36e
GoldenPig1205 <pkj7354@gmail.com>
2024-11-02 12:33:58 +0900

    러시안룰렛ㅣ초안 작성

---
+++ RGM/RGM/Modes/GameSets/RussianRoulette/RussianRoulette.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomPlayerEffects;
+using CustomRendering;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.Roles;
+using MapEditorReborn.API.Features.Objects;
+using MEC;
+using Mirror;
+using MultiBroadcast.API;
+using PlayerRoles;
+using UnityEngine;
+using Exiled.API.Enums;
+using RGM.API.Features;
+using MapEditorReborn.API.Features;
+
+namespace RGM.Modes
+{
+    class RussianRoulette
+    {
+        public static RussianRoulette Instance;
+
+        public void OnEnabled()
+        {
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            MapUtils.LoadMap("ru");
+
+            yield break;
+        }
+    }
+}
