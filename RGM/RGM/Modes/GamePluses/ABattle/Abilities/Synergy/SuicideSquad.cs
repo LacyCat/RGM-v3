4415b6921f3f0c2ace1b662e414ba89f2b9f95b4
GoldenPig1205 <pkj7354@gmail.com>
2024-11-15 18:39:31 +0900

    까먹고 위 릴리즈 서버에 적용 안함

--- RGM/RGM/Modes/GamePluses/ABattle/Abilities/Synergy/SuicideSquad.cs
+++ RGM/RGM/Modes/GamePluses/ABattle/Abilities/Synergy/SuicideSquad.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.API.Features.Items;
+using Exiled.API.Features.Pickups;
 using Exiled.Events.EventArgs.Player;
 using InventorySystem.Items.Usables.Scp330;
 using MEC;
@@ -12,19 +13,29 @@ using UnityEngine;
 
 namespace RGM.Modes.Abilities.Synergy;
 
-[RequiresAbility(ㅎㅊㄹ)]
+[RequiresAbility(AbilityType.RARE_BOMBERMAN, AbilityType.EPIC_SUICIDEBOMBER)]
 [Ability("수어사이드 스쿼드", "<봄버맨, 수어사이드 봄버맨> 사망할 경우 SCP-018를 3개 떨어트립니다.", AbilityCategory.Synergy, AbilityType.SYNERGY_SURVIVALEXPERT)]
 public class SuicideSquad : Ability
 {
     public override void OnEnabled()
     {
-        Owner.Health = Owner.Health * 5;
-
-        if (Owner.MaxHealth < Owner.Health)
-            Owner.MaxHealth = Owner.Health;
+        Exiled.Events.Handlers.Player.Died += OnDied;
     }
 
     public override void OnDisabled()
     {
+        Exiled.Events.Handlers.Player.Died -= OnDied;
+    }
+
+    public void OnDied(DiedEventArgs ev)
+    {
+        if (ev.Player != Owner)
+            return;
+
+        for (int i = 0; i < 3; i++)
+        {
+            var scp018 = Pickup.Create(ItemType.SCP018);
+            scp018.Spawn(Owner.Position, Quaternion.identity);
+        }
     }
 }
