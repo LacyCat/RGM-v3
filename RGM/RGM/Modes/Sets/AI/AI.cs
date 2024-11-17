9af91ba7f7a5b35b5b07911bae894a18f33cb1ea
GoldenPig1205 <pkj7354@gmail.com>
2024-11-17 21:38:51 +0900

    섬뜩한 힘ㅣ버그 수정 및 모드 구성 변경 (데미지 경감 95%)

--- RGM/RGM/Modes/Sets/AI/AI.cs
+++ RGM/RGM/Modes/Sets/AI/AI.cs
@@ -21,6 +21,7 @@ using static RGM.Variables.ServerManagers;
 using RGM.API.DataBases;
 using Respawning;
 using PlayerRoles.FirstPersonControl;
+using Exiled.Events.EventArgs.Player;
 
 namespace RGM.Modes
 {
@@ -28,7 +29,7 @@ namespace RGM.Modes
     class AI : Mode
     {
         public override string Name => "섬뜩한 힘";
-        public override string Description => "SCP들이 전부 A.I.로 대체되고 최대 체력이 10배로 조정됩니다. (최대 4개체)";
+        public override string Description => "SCP들이 전부 A.I.로 대체되고 데미지를 5%만 받습니다. (최대 4개체)";
         public override string Detail =>
 """
 <b>[A.I. 리스트]</b>
@@ -43,6 +44,8 @@ namespace RGM.Modes
 
         public override void OnEnabled()
         {
+            Exiled.Events.Handlers.Player.Hurting += OnHurting;
+
             Timing.RunCoroutine(OnModeStarted());
             Timing.RunCoroutine(BlockFalldown());
         }
@@ -53,20 +56,7 @@ namespace RGM.Modes
                 p.Role.Set(RoleTypeId.ClassD);
 
             foreach (var aiscprole in Datas.AIRoles)
-            {
                 Server.ExecuteCommand($"/spawnai {aiscprole.ToString()}");
-
-                yield return Timing.WaitForSeconds(0.1f);
-            }
-
-            Timing.CallDelayed(1, () =>
-            {
-                foreach (var ai in Player.List.Where(x => x.IsScp))
-                {
-                    ai.MaxHealth *= 10;
-                    ai.Health = ai.MaxHealth;
-                }
-            });
 
             yield break;
         }
@@ -78,11 +68,17 @@ namespace RGM.Modes
                 foreach (var ai in Player.List.Where(x => x.IsNPC))
                 {
                     if (ai.Position.y < -2000)
-                        ai.Position = Tools.GetRandomValue(Player.List.Where(x => x.IsNPC && x != ai).Select(x => x.Position).ToList());
+                        Server.ExecuteCommand($"/tpx {ai.Id} {Tools.GetRandomValue(Player.List.Where(x => x.IsNPC && x != ai).ToList()).Id}");
                 }
 
                 yield return Timing.WaitForSeconds(1f);
             }
+        }
+
+        public void OnHurting(HurtingEventArgs ev)
+        {
+            if (ev.Player.IsScp)
+                ev.DamageHandler.Damage /= 20;
         }
     }
 }
