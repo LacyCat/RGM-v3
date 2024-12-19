bd633f7ac920ced8c578c98c81fdfec8b79e53d5
박경현 <pkj7354@gmail.com>
2024-12-19 11:39:53 +0900

    핵이 터지기 직전 무적 상태였고, 엘레베이터에 타고 있었다면 생존하는 버그 수정

--- RGM/RGM/EventArgs/WarheadEvents.cs
+++ RGM/RGM/EventArgs/WarheadEvents.cs
@@ -20,10 +20,12 @@ namespace RGM.EventArgs
 
         public static void OnDetonating(Exiled.Events.EventArgs.Warhead.DetonatingEventArgs ev)
         {
-            foreach (var player in Player.List)
+            foreach (var player in Player.List.Where(x => x.Zone != ZoneType.Surface || x.CurrentRoom.Type.ToString().Contains("Elevator")))
             {
                 if (GodModePlayers.Contains(player))
                     GodModePlayers.Remove(player);
+
+                player.Kill(UnityEngine.Random.Range(1, 6) == 1 ? "핵폭발이 당신을 죽음으로 가는 KTX에 태웠습니다." : "핵폭발로 인해 사망하였습니다.");
             }
         }
     }
