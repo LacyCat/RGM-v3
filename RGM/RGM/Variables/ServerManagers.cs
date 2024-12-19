cce1ca5809098e6b25ba7ae314812d7bd39bc8ab
GoldenPig1205 <pkj7354@gmail.com>
2024-12-20 00:28:00 +0900

    킬이펙트> ????? 전용 킬이펙트가 추가됨 (초안)

--- RGM/RGM/Variables/ServerManagers.cs
+++ RGM/RGM/Variables/ServerManagers.cs
@@ -39,7 +39,8 @@ namespace RGM.Variables
             {"솔라 테라", "죽음에 햇빛 한 점 들기를.."},
             {"Kerfus", "귀여운 로봇으로 도장을 찍어보세요."},
             {"은제 말뚝", "비수를 꽂는 것처럼 소름끼칩니다!"},
-            {"KO 사인", "넉 다운! 상대를 쓰러트리세요!"}
+            {"KO 사인", "넉 다운! 상대를 쓰러트리세요!"},
+            {"크리스마스 트리", "축제를 돋보이게 하는 아담한 트리입니다."}
         };
         public static Dictionary<string, string> Customizations = new Dictionary<string, string>()
         {
