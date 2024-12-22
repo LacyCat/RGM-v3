24d8ed6fb4a5234643bc3ac2a19797c6517c8662
GoldenPig1205 <pkj7354@gmail.com>
2024-12-22 12:24:50 +0900

    워업ㅣ"잠수부" 능력 상시 적용

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Epic/Diver.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Epic/Diver.cs
@@ -15,13 +15,27 @@ namespace RGM.Modes.Abilities.Epic;
 [Ability("잠수부", "시야가 개선되고 스테미나가 줄어들지 않습니다.", AbilityCategory.Epic, AbilityType.EPIC_DIVER)]
 public class Diver : Ability
 {
+    CoroutineHandle _diverRotation;
+
     public override void OnEnabled()
     {
-        Owner.EnableEffect(EffectType.Invigorated);
+        _diverRotation = Timing.RunCoroutine(DiverRotation());
     }
 
     public override void OnDisabled()
     {
+        Timing.KillCoroutines(_diverRotation);
+
         Owner.DisableEffect(EffectType.Invigorated);
+    }
+
+    public IEnumerator<float> DiverRotation()
+    {
+        while (true)
+        {
+            Owner.EnableEffect(EffectType.Invigorated);
+
+            yield return Timing.WaitForSeconds(1f);
+        }
     }
 }
