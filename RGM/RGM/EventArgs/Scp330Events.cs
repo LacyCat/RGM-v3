06f2abcffb77df9a9826e904477272acd4435461
박경현 <pkj7354@gmail.com>
2024-12-23 12:18:14 +0900

    기믹 적용 메세지 비활성화

--- RGM/RGM/EventArgs/Scp330Events.cs
+++ RGM/RGM/EventArgs/Scp330Events.cs
@@ -1,5 +1,6 @@
 ﻿using Exiled.API.Features.Items;
 using InventorySystem.Items.Usables.Scp330;
+using MEC;
 using MultiBroadcast.API;
 using System;
 using System.Collections.Generic;
@@ -13,12 +14,15 @@ namespace RGM.EventArgs
     {
         public static void OnInteractingScp330(Exiled.Events.EventArgs.Scp330.InteractingScp330EventArgs ev)
         {
-            if (UnityEngine.Random.Range(1, 21) == 1)
+            Timing.CallDelayed(Timing.WaitForOneFrame, () =>
             {
-                ev.Player.TryAddCandy(CandyKindID.Pink);
+                if (UnityEngine.Random.Range(1, 21) == 1)
+                {
+                    ev.Scp330.RemoveCandy(ev.Candy);
 
-                ev.Player.AddBroadcast(10, $"<color=#FF00FF>핑크 캔디(5%, 정규)</color> 기믹이 적용되었습니다.");
-            }
+                    ev.Player.TryAddCandy(CandyKindID.Pink);
+                }
+            });
         }
     }
 }
