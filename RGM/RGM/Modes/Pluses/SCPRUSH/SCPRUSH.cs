3355b4758a8e8b34e1a5449f5b366643415b77aa
GoldenPig1205 <pkj7354@gmail.com>
2024-12-23 12:18:40 +0900

    SCP-3114 대체 또는 패치

--- RGM/RGM/Modes/Pluses/SCPRUSH/SCPRUSH.cs
+++ RGM/RGM/Modes/Pluses/SCPRUSH/SCPRUSH.cs
@@ -39,7 +39,10 @@ SCP-3114도 동일한 확률로 러쉬에 참여할 수 있습니다.
                 RoleTypeId.Scp173,
                 RoleTypeId.Scp939,
                 RoleTypeId.Scp079,
-                RoleTypeId.Scp3114,
+                // RoleTypeId.Scp3114,
+                RoleTypeId.Flamingo,
+                RoleTypeId.AlphaFlamingo,
+                RoleTypeId.ZombieFlamingo,
             };
             RoleTypeId RandomScpRole = ScpRoles[UnityEngine.Random.Range(0, ScpRoles.Count)];
 
