325567b668e408b5d4836858f778c29bea762c13
GoldenPig1205 <pkj7354@gmail.com>
2024-12-24 23:38:16 +0900

    무덤ㅣ일시적으로 격리 (탱탱볼 미스폰)

--- RGM/RGM/Modes/Sets/Cell/Cell.cs
+++ RGM/RGM/Modes/Sets/Cell/Cell.cs
@@ -13,7 +13,7 @@ using UnityEngine;
 
 namespace RGM.Modes
 {
-    [Mode(ModeCategory.Public, ModeInfo.Set, ModeType.Cell)]
+    [Mode(ModeCategory.Private, ModeInfo.Set, ModeType.Cell)]
     class Cell : Mode
     {
         public override string Name => "고문";
