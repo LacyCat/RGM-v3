93f8150ea196aab1ce34344bcb5ceedb92bb8f02
GoldenPig1205 <pkj7354@gmail.com>
2024-12-27 00:01:54 +0900

    랜덤게임모드ㅣv3.9.20 릴리즈

--- RGM/Plugin.cs
+++ RGM/Plugin.cs
@@ -35,7 +35,7 @@ namespace RGM
 
         public override string Name => "RGM";
         public override string Author => "GoldenPig1205";
-        public override Version Version { get; } = new(3, 9, 19);
+        public override Version Version { get; } = new(3, 9, 20);
         public override Version RequiredExiledVersion { get; } = new(1, 2, 0, 5);
 
         public override void OnEnabled()
