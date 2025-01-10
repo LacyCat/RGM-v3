e90312f2e9e405fbe00576a9e6967056a3b97d24
GoldenPig1205 <pkj7354@gmail.com>
2025-01-10 13:45:37 +0900

    핫픽스 -> 미니게임ㅣ"스플리프" 임시 격리 조치

--- RGM/RGM/Modes/Sets/MiniGames/MiniGames.cs
+++ RGM/RGM/Modes/Sets/MiniGames/MiniGames.cs
@@ -73,7 +73,7 @@ zombie2
             // "puzzle",
             "race",
             "light",
-            "spleef",
+            // "spleef",
             "tag",
             "tdm",
             "lava",
