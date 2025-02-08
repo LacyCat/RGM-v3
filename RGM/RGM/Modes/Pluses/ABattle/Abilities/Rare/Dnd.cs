2db3a2d831cb9450448af16db853ad768fda2df0
GoldenPig1205 <pkj7354@gmail.com>
2025-02-08 12:27:27 +0900

    워업ㅣ"자리 비움" 능력 추가

---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Rare/Dnd.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using PlayerRoles;
+using RGM.API.Features;
+using UnityEngine;
+
+using static RGM.Variables.ServerManagers;
+
+namespace RGM.Modes.Abilities.Rare;
+
+[Ability("자리 비움", "2분 동안 움직일 수 없고 아이템을 들 수 없습니다. 대신 2분 후 <color=#A4A4A4>일반</color>, <color=#2ECCFA>희귀</color>, <color=#FF00FF>영웅</color> 능력을 하나씩 획득합니다.", AbilityCategory.Rare, AbilityType.RARE_DND)]
+public class Dnd : Ability
+{
+    public override void OnEnabled()
+    {
+        IEnumerator<float> enumerator()
+        {
+            Owner.EnableEffect(EffectType.Ensnared, 1, 120);
+
+            for (int i = 0; i < 120; i++)
+            {
+                Owner.CurrentItem = null;
+
+                yield return Timing.WaitForSeconds(1f);
+            }
+
+            List<AbilityCategory> categories = new List<AbilityCategory>
+            {
+                AbilityCategory.Common,
+                AbilityCategory.Rare,
+                AbilityCategory.Epic
+            };
+
+            foreach (AbilityCategory category in categories)
+                Owner.AddAbility(ABattle.Instance.GetRandomAbilities(category, 1).GetRandomValue());
+        }
+
+        Timing.RunCoroutine(enumerator());
+    }
+
+    public override void OnDisabled()
+    {
+    }
+}
