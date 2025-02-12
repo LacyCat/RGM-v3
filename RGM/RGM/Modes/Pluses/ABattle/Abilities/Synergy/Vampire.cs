082566cb1ab5cc33217e4b0c9e989cf8efd22eff
GoldenPig1205 <pkj7354@gmail.com>
2025-02-13 00:55:35 +0900

    워업ㅣ"뱀파이어" 시너지의 회복량 조정(12% -> 6%), 자해 공갈 불가하도록 수정

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Synergy/Vampire.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Synergy/Vampire.cs
@@ -13,7 +13,7 @@ using UnityEngine;
 namespace RGM.Modes.Abilities.Synergy;
 
 [RequiresAbility(AbilityType.RARE_VAMPIRE, AbilityType.RARE_TRANSPARENTCLOAK)]
-[Ability("뱀파이어", "<흡혈귀, 투명 망토> 마침 밤이군요, 활동할 시간입니다. 피해를 입히면 피해량의 12%를 즉시 회복합니다. <b>이 능력은 최대 체력을 무시합니다.</b>", AbilityCategory.Synergy, AbilityType.SYNERGY_VAMPIRE)]
+[Ability("뱀파이어", "<흡혈귀, 투명 망토> 마침 밤이군요, 활동할 시간입니다. 피해를 입히면 피해량의 6%를 즉시 회복합니다. <b>이 능력은 최대 체력을 무시합니다.</b>", AbilityCategory.Synergy, AbilityType.SYNERGY_VAMPIRE)]
 public class Vampire : Ability
 {
     public override void OnEnabled()
@@ -28,9 +28,9 @@ public class Vampire : Ability
 
     public void OnHurting(HurtingEventArgs ev)
     {
-        if (ev.Attacker == null || ev.Attacker != Owner)
+        if (ev.Attacker == null || ev.Attacker != Owner || !HitboxIdentity.IsEnemy(ev.Attacker.ReferenceHub, ev.Player.ReferenceHub))
             return;
 
-        ev.Player.Health += (12 * ev.DamageHandler.Damage / 100);
+        ev.Player.Health += (6 * ev.DamageHandler.Damage / 100);
     }
 }
