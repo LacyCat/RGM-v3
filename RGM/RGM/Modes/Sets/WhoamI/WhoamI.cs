3e370c1e99dd19da26d7e0df4681d9f43e3ded71
GoldenPig1205 <pkj7354@gmail.com>
2025-02-13 00:57:52 +0900

    나는 누구?ㅣ버그를 고치지 않았지만 일단 릴리즈

--- RGM/RGM/Modes/Sets/WhoamI/WhoamI.cs
+++ RGM/RGM/Modes/Sets/WhoamI/WhoamI.cs
@@ -19,7 +19,7 @@ using Exiled.Events.EventArgs.Server;
 
 namespace RGM.Modes
 {
-    [Mode(ModeCategory.Private, ModeInfo.Set, ModeType.WhoamI)]
+    [Mode(ModeCategory.Public, ModeInfo.Set, ModeType.WhoamI)]
     public class WhoamI : Mode
     {
         public override string Name => "나는 누구?";
@@ -45,76 +45,65 @@ namespace RGM.Modes
         {
             while (true)
             {
-                foreach (var player in Player.List.Where(x => x.IsAlive))
+                foreach (var player in Player.List.Where(x => x.IsAlive).ToList())
                 {
-                    try
+                    RoleTypeId RoleType = player.Role.Type;
+                    float MaxHealth = player.MaxHealth;
+                    float Health = player.Health;
+                    List<StatusEffectBase> ActiveEffects = player.ActiveEffects.ToList();
+                    List<Item> Items = player.Items == null ? new List<Item>() : player.Items.ToList();
+                    Item CurrentItem = player.CurrentItem;
+                    Vector3 Position = player.Position;
+
+                    PlayerInfo pi = new PlayerInfo
                     {
-                        RoleTypeId RoleType = player.Role.Type;
-                        float MaxHealth = player.MaxHealth;
-                        float Health = player.Health;
-                        IEnumerable<StatusEffectBase> ActiveEffects = player.ActiveEffects.ToList();
-                        IReadOnlyCollection<Item> Items = player.Items == null ? new List<Item>() : player.Items.ToList();
-                        Item CurrentItem = player.CurrentItem;
-                        Vector3 Position = player.Position;
+                        RoleType = RoleType,
+                        MaxHealth = MaxHealth,
+                        Health = Health,
+                        ActiveEffects = ActiveEffects,
+                        Items = Items,
+                        CurrentItem = CurrentItem,
+                        Position = Position
+                    };
 
-                        PlayerInfo pi = new PlayerInfo
-                        {
-                            RoleType = RoleType,
-                            MaxHealth = MaxHealth,
-                            Health = Health,
-                            ActiveEffects = ActiveEffects,
-                            Items = Items,
-                            CurrentItem = CurrentItem,
-                            Position = Position
-                        };
-
-                        if (PlayersInfo.ContainsKey(player))
-                            PlayersInfo[player] = pi;
-
-                        else
-                            PlayersInfo.Add(player, pi);
-                    }
-                    catch (Exception e)
+                    if (!PlayersInfo.ContainsKey(player))
                     {
-                        Log.Error(e);
+                        PlayersInfo.Add(player, pi);
                     }
                 }
 
                 foreach (var player in PlayersInfo.Keys.ToList())
                 {
-                    try
+                    Player p = Tools.GetRandomValue(PlayersInfo.Keys.Where(x => x != player).ToList());
+
+                    player.Role.Set(PlayersInfo[p].RoleType);
+                    player.MaxHealth = PlayersInfo[p].MaxHealth;
+                    player.Health = PlayersInfo[p].Health;
+
+                    foreach (var effect in PlayersInfo[p].ActiveEffects.ToList())
+                        player.EnableEffect(effect, effect.Intensity, effect.Duration);
+
+                    player.ClearItems();
+
+                    foreach (var item in PlayersInfo[p].Items.ToList())
+                        player.AddItem(item.Type);
+
+                    if (PlayersInfo[p].CurrentItem != null)
                     {
-                        Player p = Tools.GetRandomValue(PlayersInfo.Keys.Where(x => x != player).ToList());
-
-                        player.Role.Set(PlayersInfo[p].RoleType);
-                        player.MaxHealth = PlayersInfo[p].MaxHealth;
-                        player.Health = PlayersInfo[p].Health;
-
-                        foreach (var effect in PlayersInfo[p].ActiveEffects)
-                            player.EnableEffect(effect, effect.Intensity, effect.Duration);
-
-                        player.ClearItems();
-
-                        foreach (var item in PlayersInfo[p].Items)
-                            player.AddItem(item.Type);
-
                         player.CurrentItem = player.Items.ToList().Find(x => x.Type == PlayersInfo[p].CurrentItem.Type);
-
-                        player.Position = PlayersInfo[p].Position;
-
-                        if (PlayersInfo.ContainsKey(p))
-                            PlayersInfo.Remove(p);
                     }
-                    catch (Exception e)
-                    {
-                        Log.Error(e);
-                    }
+
+                    player.Position = PlayersInfo[p].Position;
+
+                    if (PlayersInfo.ContainsKey(p))
+                        PlayersInfo.Remove(p);
                 }
 
                 PlayersInfo.Clear();
 
                 yield return Timing.WaitForSeconds(60f);
             }
+
         }
 
         public void OnRoundEnded(RoundEndedEventArgs ev)
