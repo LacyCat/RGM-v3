8cbb28e9f9d52f92dd766f1f4619c7f60b02d7f7
GoldenPig1205 <pkj7354@gmail.com>
2025-02-25 00:43:10 +0900

    워업ㅣ"구사일생" 능력은 이제 3번 사용 가능합니다.

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Epic/Survivor.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Epic/Survivor.cs
@@ -15,9 +15,12 @@ using static RGM.Variables.ServerManagers;
 
 namespace RGM.Modes.Abilities.Epic;
 
-[Ability("구사일생", "사망 판정을 받을 경우, 1번 버텨내며 3초간 투명 상태와 무적이 됩니다.", AbilityCategory.Epic, AbilityType.EPIC_SURVIVOR)]
+[Ability("구사일생", "사망 판정을 받을 경우, 3초간 투명 상태와 무적이 됩니다. (최대 3번)", AbilityCategory.Epic, AbilityType.EPIC_SURVIVOR)]
 public class Survivor : Ability
 {
+    int power = 3;
+    bool isEnabled = false;
+
     public override void OnEnabled()
     {
         Exiled.Events.Handlers.Player.Dying += OnDying;
@@ -33,31 +36,43 @@ public class Survivor : Ability
         if (ev.Player != Owner || ABattle.Instance.IsLifeUsed[Owner] || Datas.BlockDamageTypes.Contains(ev.DamageHandler.Type))
             return;
 
-        ev.IsAllowed = false;
-        ev.Player.RemoveAbility(this);
-
-        ev.Player.EnableEffect(EffectType.Blinded, 1, 3);
-        ev.Player.EnableEffect(EffectType.Invisible, 1, 3);
-        ev.Player.GetEffect(EffectType.MovementBoost).Intensity += 20;
-
-        GodModePlayers.Add(ev.Player);
-
-        Timing.CallDelayed(3f, () =>
+        if (!isEnabled)
         {
-            if (ev.Player.GetEffect(EffectType.MovementBoost).Intensity >= 20)
-                ev.Player.GetEffect(EffectType.MovementBoost).Intensity -= 20;
+            isEnabled = true;
 
-            if (GodModePlayers.Contains(ev.Player))
-                GodModePlayers.Remove(ev.Player);
-        });
+            ev.IsAllowed = false;
 
-        ev.Player.ShowHint($"<color={ABattle.RatingColor["영웅"]}>구사일생</color> 능력으로 인해 3초간 죽음을 피합니다.");
+            ev.Player.EnableEffect(EffectType.Blinded, 1, 3);
+            ev.Player.EnableEffect(EffectType.Invisible, 1, 3);
+            ev.Player.GetEffect(EffectType.MovementBoost).Intensity += 20;
 
-        ABattle.Instance.IsLifeUsed[Owner] = true;
+            GodModePlayers.Add(ev.Player);
 
-        Timing.CallDelayed(Timing.WaitForOneFrame, () =>
-        {
-            ABattle.Instance.IsLifeUsed[Owner] = false;
-        });
+            Timing.CallDelayed(3f, () =>
+            {
+                if (ev.Player.GetEffect(EffectType.MovementBoost).Intensity >= 20)
+                    ev.Player.GetEffect(EffectType.MovementBoost).Intensity -= 20;
+
+                if (GodModePlayers.Contains(ev.Player))
+                    GodModePlayers.Remove(ev.Player);
+
+                isEnabled = false;
+            });
+
+            ev.Player.ShowHint($"<color={ABattle.RatingColor["영웅"]}>구사일생</color> 능력으로 인해 3초간 죽음을 피합니다. ({power - 1}번 남음)");
+
+            ABattle.Instance.IsLifeUsed[Owner] = true;
+
+            Timing.CallDelayed(Timing.WaitForOneFrame, () =>
+            {
+                ABattle.Instance.IsLifeUsed[Owner] = false;
+            });
+
+            if (power == 1)
+                ev.Player.RemoveAbility(this);
+
+            else
+                power--;
+        }
     }
 }
