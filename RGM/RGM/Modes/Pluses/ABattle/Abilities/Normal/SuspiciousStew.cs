16ceb3d07df802093b2781f88743acc66b2997be
GoldenPig1205 <pkj7354@gmail.com>
2025-03-15 01:54:51 +0900

    워업ㅣ"수상한 스튜" 효과 길이 무제한

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Normal/SuspiciousStew.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Normal/SuspiciousStew.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace RGM.Modes.Abilities.Normal;
 
-[Ability("수상한 스튜", "적용 시간과 강도가 랜덤인 무작위 효과를 하나 받습니다.", AbilityCategory.Common, AbilityType.NORMAL_SUSPICIOUSSTEW)]
+[Ability("수상한 스튜", "효과 세기가 랜덤인 영구 지속 무작위 효과를 하나 받습니다.", AbilityCategory.Common, AbilityType.NORMAL_SUSPICIOUSSTEW)]
 public class SuspiciousStew : Ability
 {
     public override void OnEnabled()
@@ -18,10 +18,8 @@ public class SuspiciousStew : Ability
 
         EffectType Effect = Tools.GetRandomValue(Effects.Where(x => x != EffectType.PocketCorroding).ToList());
         byte Intensity = (byte)Random.Range(1, Random.Range(12, Random.Range(48, Random.Range(64, Random.Range(100, 255)))));
-        float Duration = Random.Range(1, Random.Range(12, Random.Range(24, Random.Range(48, 61))));
 
-        Owner.EnableEffect(Effect, Intensity, Duration);
-        Owner.ShowHint($"<color=#D0FA58>{Effect}</color> 효과가 {Intensity}만큼 {Duration}초 동안 적용되는 중입니다..", Duration);
+        Owner.EnableEffect(Effect, Intensity);
     }
 
     public override void OnDisabled()
