8b03e273b0cc8da1c8db21592b05be8f3950109b
GoldenPig1205 <pkj7354@gmail.com>
2025-03-16 13:30:14 +0900

    야간 작전ㅣ첫 릴리즈 (Plus)

---
+++ RGM/RGM/Modes/Pluses/Clairvoyance/Clairvoyance.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomRendering;
+using Exiled.API.Features;
+using MEC;
+using Mirror;
+using RGM.API.Features;
+using RGM.API.DataBases;
+using UnityEngine;
+using Exiled.API.Features.Items;
+using Exiled.API.Enums;
+
+namespace RGM.Modes
+{
+    [Mode(ModeCategory.Public, ModeInfo.Plus, ModeType.Clairvoyance)]
+    class Clairvoyance : Mode
+    {
+        public override string Name => "야간 작전";
+        public override string Description => "모두가 SCP-1344 효과를 받습니다.";
+        public override string Detail =>
+"""
+SCP-1344 효과 - 투시
+""";
+        public override string Color => "F4FA58";
+
+        public static Clairvoyance Instance;
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Spawned += OnSpawned;
+
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            foreach (var player in Player.List)
+            {
+                Spawned(player);
+            }
+
+            yield break;
+        }
+
+        public void OnSpawned(Exiled.Events.EventArgs.Player.SpawnedEventArgs ev)
+        {
+            Spawned(ev.Player);
+        }
+
+        public void Spawned(Player player)
+        {
+            player.EnableEffect(EffectType.Scp1344);
+        }
+    }
+}
