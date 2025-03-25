986e8b7a35aca2383c06343aa3a9521225252eea
GoldenPig1205 <pkj7354@gmail.com>
2025-03-26 00:53:47 +0900

    워업ㅣ"성직자", "황소" 능력 추가

---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Epic/Priest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using RGM.API.DataBases;
+using RGM.API.Features;
+using UnityEngine;
+
+using static RGM.Variables.ServerManagers;
+
+namespace RGM.Modes.Abilities.Epic;
+
+[Ability("성직자", "관전석에서 3명을 뽑아 아군으로 만들고, 자신의 위치로 소환합니다.", AbilityCategory.Epic, AbilityType.EPIC_PRIEST)]
+public class Priest : Ability
+{
+    public override void OnEnabled()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            var dead = Player.List.Where(x => x.IsDead).ToList();
+
+            if (dead.Count() != 0)
+            {
+                var revive = dead.GetRandomValue();
+
+                revive.Role.Set(Owner.Role.Type);
+                revive.Position = Owner.Position;
+            }
+        }
+    }
+
+    public override void OnDisabled()
+    {
+
+    }
+}
---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Normal/Rush.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using PlayerRoles;
+using RGM.API.Features;
+using UnityEngine;
+
+namespace RGM.Modes.Abilities.Normal;
+
+[Ability("황소", "지급된 동전을 튕기면 1초 동안 속도가 압도적으로 빨라집니다.", AbilityCategory.Common, AbilityType.NORMAL_RUSH)]
+public class Rush : Ability
+{
+    ushort Serial = 0;
+
+    public override void OnEnabled()
+    {
+        Item coin = Owner.AddItem(ItemType.Coin);
+        Serial = coin.Serial;
+
+        Exiled.Events.Handlers.Player.ChangedItem += OnChangedItem;
+        Exiled.Events.Handlers.Player.FlippingCoin += OnFlippingCoin;
+    }
+
+    public override void OnDisabled()
+    {
+    }
+
+    public void OnChangedItem(ChangedItemEventArgs ev)
+    {
+        if (ev.Item != null)
+        {
+            if (Serial == ev.Item.Serial)
+                ev.Player.ShowHint($"이 동전을 튕기면 <b><color={ABattle.RatingColor["일반"]}>황소</color></b> 능력을 사용할 수 있습니다.");
+        }
+    }
+
+    public void OnFlippingCoin(FlippingCoinEventArgs ev)
+    {
+        if (Serial == ev.Item.Serial)
+        {
+            ev.Item.Destroy();
+
+            var effect = ev.Player.GetEffect(EffectType.MovementBoost);
+
+            ev.Player.EnableEffect(EffectType.MovementBoost, 255, 1);
+
+            Timing.CallDelayed(1, () =>
+            {
+                ev.Player.EnableEffect(effect);
+            });
+        }
+    }
+}
