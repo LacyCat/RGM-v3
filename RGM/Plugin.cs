bb103956f3889176d54194f5c38ea8479dc80641
GoldenPig1205 <pkj7354@gmail.com>
2025-03-31 23:55:19 +0900

    릴리즈 v4.1.41 <랜덤게임모드

--- RGM/Plugin.cs
+++ RGM/Plugin.cs
@@ -36,7 +36,7 @@ namespace RGM
 
         public override string Name => "RGM";
         public override string Author => "GoldenPig1205";
-        public override Version Version { get; } = new(3, 13, 5);
+        public override Version Version { get; } = new(4, 1, 41);
         public override Version RequiredExiledVersion { get; } = new(1, 2, 0, 5);
 
         public override void OnEnabled()
@@ -99,6 +99,7 @@ namespace RGM
             Exiled.Events.Handlers.Player.DroppedItem += OnDroppedItem;
             Exiled.Events.Handlers.Player.DroppedAmmo += OnDroppedAmmo;
             Exiled.Events.Handlers.Player.ItemAdded += OnItemAdded;
+            Exiled.Events.Handlers.Player.Shot += OnShot;
             Exiled.Events.Handlers.Player.Kicking += OnKicking;
             Exiled.Events.Handlers.Player.Banning += OnBanning;
             Exiled.Events.Handlers.Player.ChangingGroup += OnChangingGroup;
@@ -138,6 +139,7 @@ namespace RGM
             Exiled.Events.Handlers.Player.DroppedItem -= OnDroppedItem;
             Exiled.Events.Handlers.Player.DroppedAmmo -= OnDroppedAmmo;
             Exiled.Events.Handlers.Player.ItemAdded -= OnItemAdded;
+            Exiled.Events.Handlers.Player.Shot -= OnShot;
             Exiled.Events.Handlers.Player.Kicking -= OnKicking;
             Exiled.Events.Handlers.Player.Banning -= OnBanning;
             Exiled.Events.Handlers.Player.ChangingGroup -= OnChangingGroup;
