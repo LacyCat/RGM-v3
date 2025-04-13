dff5f955a50365d95d62158000122e7465054dbb
GoldenPig1205 <pkj7354@gmail.com>
2025-04-13 18:17:01 +0900

    스플리프ㅣ정상적인 플레이를 꼼수로 판명하던 버그 수정

--- RGM/RGM/Modes/Sets/Spleef/Spleef.cs
+++ RGM/RGM/Modes/Sets/Spleef/Spleef.cs
@@ -158,7 +158,7 @@ namespace RGM.Modes
                     }
                 }
 
-                yield return Timing.WaitForOneFrame;
+                yield return Timing.WaitForSeconds(1);
             }
         }
 
