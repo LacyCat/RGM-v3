5bce02faf6d6dc5b02b5f21551f1ce9f9b2c737e
박경현 <pkj7354@gmail.com>
2025-05-07 13:06:04 +0900

    워업ㅣ"매드 사이언티스트" 능력 추가

---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Epic/MadScientist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using PlayerRoles;
+using RGM.API.DataBases;
+using RGM.API.Features;
+using UnityEngine;
+
+using static RGM.Variables.ServerManagers;
+
+namespace RGM.Modes.Abilities.Epic;
+
+[Ability("매드 사이언티스트", "사망 시, 10초 뒤에 해당 자리에서 리셋됩니다. 랜덤한 능력 5개가 지급됩니다.", AbilityCategory.Epic, AbilityType.EPIC_MADSCIENTIST)]
+public class MadScientist : Ability
+{
+    public override void OnEnabled()
+    {
+        Exiled.Events.Handlers.Player.Died += OnDied;
+    }
+
+    public override void OnDisabled()
+    {
+        Exiled.Events.Handlers.Player.Died -= OnDied;
+    }
+
+    public void OnDied(DiedEventArgs ev)
+    {
+        Timing.CallDelayed(10, () =>
+        {
+            ev.Player.Role.Set(ev.TargetOldRole, RoleSpawnFlags.AssignInventory);
+
+            for (int i = 0; i < 5; i++)
+                Owner.AddAbility(ABattle.Instance.GetRandomAbilities(ABattle.Instance.GetCategory(Owner), 1)[0]);
+        });
+    }
+}
