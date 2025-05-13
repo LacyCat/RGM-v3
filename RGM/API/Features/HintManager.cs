0ec06887eee0a126bb216345420c491743c1246d
박경현 <pkj7354@gmail.com>
2025-05-13 14:11:48 +0900

    힌트) Hint에 대한 매커니즘을 변경하여, 한번에 2개 이상의 힌트를 보이도록 함

---
+++ RGM/API/Features/HintManager.cs
@@ -0,0 +1,44 @@
+﻿using Exiled.API.Features;
+using MEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGM.API.Features
+{
+    public static class HintManager
+    {
+        private static Dictionary<Player, List<string>> _playerHints = new Dictionary<Player, List<string>>();
+
+        public static IEnumerator<float> OnStarted()
+        {
+            while (true)
+            {
+                foreach (var player in Player.List.Where(x => x.IsAlive && _playerHints.ContainsKey(x) && -_playerHints[x].Count > 0))
+                {
+                    player.ShowHint($"{string.Join("\n", _playerHints[player])}", 1.2f);
+                }
+
+                yield return Timing.WaitForSeconds(1);
+            }
+        }
+
+        public static void AddHint(this Player player, string hint, float duration = 3)
+        {
+            if (!_playerHints.ContainsKey(player))
+                _playerHints[player] = new List<string>();
+
+            if (!_playerHints[player].Contains(hint))
+            {
+                _playerHints[player].Add(hint);
+
+                Timing.CallDelayed(duration, () =>
+                {
+                    _playerHints[player].Remove(hint);
+                });
+            }
+        }
+    }
+}
