3afe73c4e6c1e33141aa3bbd7a449662d1680bba
GoldenPig1205 <pkj7354@gmail.com>
2025-05-16 00:44:04 +0900

    워업ㅣ"부자Ⅰ" 시너지가 주는 랜덤코인 감소(3 -> 1)

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Synergy/Rich1.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Synergy/Rich1.cs
@@ -14,14 +14,14 @@ using UnityEngine;
 namespace RGM.Modes.Abilities.Synergy;
 
 [RequiresAbility(AbilityType.NORMAL_ESCAPE, AbilityType.NORMAL_MILK, AbilityType.NORMAL_RUSH)]
-[Ability("부자Ⅰ", "<위기 탈출, 우유, 황소> 랜덤코인 3개를 얻으세요.", AbilityCategory.Synergy, AbilityType.SYNERGY_RICH1)]
+[Ability("부자Ⅰ", "<위기 탈출, 우유, 황소> 랜덤코인 1개를 얻으세요.", AbilityCategory.Synergy, AbilityType.SYNERGY_RICH1)]
 public class Rich1 : Ability
 {
     public override void OnEnabled()
     {
         List<string> uc = UsersManager.UsersCache[Owner.UserId];
 
-        Owner.SetRC(Owner.UserId, 3 + int.Parse(uc[1]), out string response);
+        Owner.SetRC(Owner.UserId, 1 + int.Parse(uc[1]), out string response);
     }
 
     public override void OnDisabled()
