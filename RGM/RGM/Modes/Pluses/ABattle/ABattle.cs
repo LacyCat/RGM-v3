273a4a8ff0466ee33d761e64b62af34904b0371a
박경현 <pkj7354@gmail.com>
2025-05-28 16:28:20 +0900

    워업ㅣ"쇼맨십" 능력 추가

--- RGM/RGM/Modes/Pluses/ABattle/ABattle.cs
+++ RGM/RGM/Modes/Pluses/ABattle/ABattle.cs
@@ -84,12 +84,13 @@ public class ABattle : Mode
         {"기본", "워크스테이션 업그레이드를 즐기세요!"},
         {"1 + 1", "능력 선택창에 등장하는 능력의 수가 1개인 대신, 동일한 등급의 능력을 1개를 더 받습니다."},
         {"수저", "능력 선택창에서 등장하는 능력의 수가 최대 5개까지 늘어날 수 있습니다."},
-        //{"골드 전주곡", $"스폰 즉시 <color={RatingColor["영웅"]}>영웅</color> 등급의 능력을 얻습니다."},
-        //{"프리즘 전주곡", $"스폰 즉시 <color={RatingColor["영웅"]}>영웅</color> 등급의 능력을 얻습니다. 낮은 확률로 <color={RatingColor["전설"]}>전설</color>, <color={RatingColor["신화"]}>신화</color> 등급의 능력이 지급될 수 있습니다."},
+            //{"골드 전주곡", $"스폰 즉시 <color={RatingColor["영웅"]}>영웅</color> 등급의 능력을 얻습니다."},
+            //{"프리즘 전주곡", $"스폰 즉시 <color={RatingColor["영웅"]}>영웅</color> 등급의 능력을 얻습니다. 낮은 확률로 <color={RatingColor["전설"]}>전설</color>, <color={RatingColor["신화"]}>신화</color> 등급의 능력이 지급될 수 있습니다."},
         {"잔칫상", $"<color={RatingColor["희귀"]}>희귀</color> 이상 등급의 능력이 등장할 확률이 높아집니다."},
         {"스펙업", "능력을 획득하면 추가 최대 체력이 지급됩니다. (+10 (SCP의 경우 +50))"},
         {"캐시 청소", "9분마다 모든 유저의 워크스테이션 획득 기록이 초기화됩니다."},
-        {"대출", "워크스테이션 제한이 해제됩니다. 각 워크스테이션마다 처음 1회를 제외하고 추가로 얻으려고 시도하는 경우, 20% 확률로 아사합니다."}
+        {"대출", "워크스테이션 제한이 해제됩니다. 각 워크스테이션마다 처음 1회를 제외하고 추가로 얻으려고 시도하는 경우, 20% 확률로 아사합니다."},
+        {"지원", "1~3분마다 모두에게 능력 선택창이 열립니다."}
     };
 
     public static string ColorFormat(string text)
@@ -123,6 +124,9 @@ public class ABattle : Mode
 
             if (extraMode == "캐시 청소")
                 Timing.RunCoroutine(Instance.ClearCache());
+
+            if (extraMode == "지원")
+                Timing.RunCoroutine(Instance.Backup());
 
             return extraMode;
         }
@@ -242,6 +246,19 @@ public class ABattle : Mode
             }
 
             yield return Timing.WaitForSeconds(60 * 9);
+        }
+    }
+
+    private IEnumerator<float> Backup()
+    {
+        while (true)
+        {
+            foreach (var player in Player.List.Where(x => !x.IsNPC && x.IsAlive))
+            {
+                StartSelect(player);
+            }
+
+            yield return Timing.WaitForSeconds(Random.Range(1, 4) * 60);
         }
     }
 
---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Unique/scp3114/Showmanship.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks.Sources;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.API.Features.Roles;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Scp106;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using RGM.API.DataBases;
+using RGM.API.Features;
+using UnityEngine;
+
+namespace RGM.Modes.Abilities.Unique.Scp3114;
+
+[Ability("쇼맨쉽", "춤추는 동안 지속적인 데미지를 받습니다. 대신 외부로부터 받는 데미지를 66% 줄입니다. <size=10>idea by 조용히게임함</size>", AbilityCategory.Scp3114, AbilityType.SCP3114_SHOWMANSHIP)]
+public class Showmanship : Ability
+{
+    CoroutineHandle coroutine;
+
+    public override void OnEnabled()
+    {
+        Exiled.Events.Handlers.Player.Hurting += OnHurting;
+
+        coroutine = Timing.RunCoroutine(onStarted());
+    }
+
+    public override void OnDisabled()
+    {
+        Exiled.Events.Handlers.Player.Hurting -= OnHurting;
+
+        Timing.KillCoroutines(coroutine);
+    }
+
+    public IEnumerator<float> onStarted()
+    {
+        while (true)
+        {
+            Scp3114Role scp3114 = (Scp3114Role)Owner.Role;
+
+            if (scp3114.Dance)
+            {
+                Owner.Hit(Owner, 0.2f);
+            }
+
+            yield return Timing.WaitForSeconds(0.1f);
+        }
+    }
+
+    public void OnHurting(HurtingEventArgs ev)
+    {
+        if (ev.Player != Owner)
+            return;
+
+        Scp3114Role scp3114 = (Scp3114Role)ev.Player.Role;
+
+        if (scp3114.Dance)
+        {
+            ev.DamageHandler.Damage /= 3;
+        }
+    }
+}
