82ac176c5e6a8147431127cc1a9147e4d762f0ac
GoldenPig1205 <pkj7354@gmail.com>
2025-06-04 23:15:10 +0900

    워업ㅣ"중복 기연" 시너지의 발생 조건 수정 (반드시 선택창에 3개 이상)

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Synergy/DuplicateFate.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Synergy/DuplicateFate.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 
 namespace RGM.Modes.Abilities.Synergy;
 
-[Ability("중복 기연", "<능력 선택지가 모두 중복일 경우> 당신은 해당 능력과 인연을 맺은 것 같습니다. 3개 전부 다 드릴게요!", AbilityCategory.Synergy, AbilityType.SYNERGY_DUPLICATEFATE)]
+[Ability("중복 기연", "<능력 선택지가 모두 중복일 경우> 당신은 해당 능력과 인연을 맺은 것 같습니다.", AbilityCategory.Synergy, AbilityType.SYNERGY_DUPLICATEFATE)]
 public class DuplicateFate : Ability
 {
     public override void OnEnabled()
