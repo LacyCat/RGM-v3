c19a63dcf2d20c63ff436715ff46f07f219af158
GoldenPig1205 <pkj7354@gmail.com>
2025-06-04 23:15:46 +0900

    워업ㅣ"고대의 존재 압도" 설명 수정

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Unique/scp079/Overwhelming.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Unique/scp079/Overwhelming.cs
@@ -17,7 +17,7 @@ using UnityEngine;
 
 namespace RGM.Modes.Abilities.Unique.Scp079;
 
-[Ability("고대의 존재 압도", "[Q]ㅣ키를 홀드하는 동안 전력을 사용하여 해당 방에 있는 인간의 속도를 감소시킵니다.", AbilityCategory.Scp079, AbilityType.SCP079_OVERWHELMING)]
+[Ability("고대의 존재 압도", "[Q] 또는 [V]ㅣ키를 홀드하는 동안 전력을 사용하여 해당 방에 있는 인간의 속도를 감소시킵니다.", AbilityCategory.Scp079, AbilityType.SCP079_OVERWHELMING)]
 public class Overwhelming : Ability
 {
     public override void OnEnabled()
