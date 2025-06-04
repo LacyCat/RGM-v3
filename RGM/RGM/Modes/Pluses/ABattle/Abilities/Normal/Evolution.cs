deb68d5ee6674f118df0d35135a072de12736877
GoldenPig1205 <pkj7354@gmail.com>
2025-06-04 23:15:35 +0900

    워업ㅣ"진화" 능력 너프 (12% -> 9%)

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Normal/Evolution.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Normal/Evolution.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 namespace RGM.Modes.Abilities.Normal;
 
-[Ability("진화", "몸의 크기가 12% 작아집니다.", AbilityCategory.Common, AbilityType.NORMAL_EVOLUTION)]
+[Ability("진화", "몸의 크기가 9% 작아집니다.", AbilityCategory.Common, AbilityType.NORMAL_EVOLUTION)]
 public class Evolution : Ability
 {
     public override void OnEnabled()
     {
-        Owner.Scale = new Vector3(Owner.Scale.x - 0.12f, Owner.Scale.y - 0.12f, Owner.Scale.z - 0.12f);
+        Owner.Scale = new Vector3(Owner.Scale.x - 0.09f, Owner.Scale.y - 0.09f, Owner.Scale.z - 0.09f);
     }
 
     public override void OnDisabled()
     {
-        Owner.Scale = new Vector3(Owner.Scale.x + 0.12f, Owner.Scale.y + 0.12f, Owner.Scale.z + 0.12f);
+        Owner.Scale = new Vector3(Owner.Scale.x + 0.09f, Owner.Scale.y + 0.09f, Owner.Scale.z + 0.09f);
     }
 }
