e7466e9bb42ea7b00b7deec2c3435b285c9f5c5e
GoldenPig1205 <pkj7354@gmail.com>
2025-06-08 23:32:13 +0900

    킬스트릭ㅣ인간에게는 능력 5개 선택창 띄워주고 시작

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Legend/KillStreak.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Legend/KillStreak.cs
@@ -12,17 +12,34 @@ using UnityEngine;
 
 namespace RGM.Modes.Abilities.Legend;
 
-[Ability("킬스트릭", "적을 처치할 때마다 새로운 능력을 얻습니다.", AbilityCategory.Legend, AbilityType.LEGEND_KILLSTREAK)]
+[Ability("킬스트릭", "적을 처치할 때마다 새로운 능력을 얻습니다. (인간 진영의 경우 능력 5개를 추가로 선택합니다.)", AbilityCategory.Legend, AbilityType.LEGEND_KILLSTREAK)]
 public class KillStreak : Ability
 {
+    CoroutineHandle coroutine;
+
     public override void OnEnabled()
     {
         Exiled.Events.Handlers.Player.Died += OnDied;
+
+        coroutine = Timing.RunCoroutine(onStarted());
     }
 
     public override void OnDisabled()
     {
         Exiled.Events.Handlers.Player.Died -= OnDied;
+
+        Timing.KillCoroutines(coroutine);
+    }
+
+    public IEnumerator<float> onStarted()
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            ABattle.Instance.StartSelect(Owner);
+
+            while (ABattle.Instance.IsSelecting[Owner])
+                yield return Timing.WaitForOneFrame;
+        }
     }
 
     public void OnDied(DiedEventArgs ev)
