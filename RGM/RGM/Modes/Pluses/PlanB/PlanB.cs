6b30e90bf77a07179b4a2ea2723f0039e975356c
GoldenPig1205 <pkj7354@gmail.com>
2025-06-25 23:33:41 +0900

    플랜 BㅣSCP도 이제 동전 구멍에 동전을 튕기면 자판기 효과를 받습니다.

--- RGM/RGM/Modes/Pluses/PlanB/PlanB.cs
+++ RGM/RGM/Modes/Pluses/PlanB/PlanB.cs
@@ -17,6 +17,7 @@ using Exiled.Events.EventArgs.Player;
 using InventorySystem.Items.Usables.Scp330;
 using Exiled.API.Features.Pickups;
 using Exiled.API.Enums;
+using static PlayerList;
 
 namespace RGM.Modes
 {
@@ -45,7 +46,8 @@ namespace RGM.Modes
         public override void OnEnabled()
         {
             Exiled.Events.Handlers.Player.SearchingPickup += OnSearchingPickup;
-            Exiled.Events.Handlers.Player.Died += OnDied;
+            Exiled.Events.Handlers.Player.FlippingCoin += OnFlippingCoin;
+            Exiled.Events.Handlers.Player.Dying += OnDying;
 
             Timing.RunCoroutine(OnModeStarted());
         }
@@ -56,9 +58,17 @@ namespace RGM.Modes
 
             foreach (var pickup in Pickup.List)
             {
-                if (UnityEngine.Random.Range(1, 11) == 1)
+                try
                 {
-                    Pickup.CreateAndSpawn(ItemType.Coin, pickup.Transform.position + new Vector3(0, 0.1f, 0));
+                    if (UnityEngine.Random.Range(1, 11) == 1)
+                    {
+                        Item coin = Item.Create(ItemType.Coin);
+                        coin.CreatePickup(pickup.Transform.position + new Vector3(0, 0.1f, 0));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Error($"[PlanB] Error while creating coin: {e}");
                 }
             }
 
@@ -146,11 +156,97 @@ namespace RGM.Modes
             }
         }
 
-        public void OnDied(DiedEventArgs ev)
+        public IEnumerator<float> OnFlippingCoin(FlippingCoinEventArgs ev)
+        {
+            if (Physics.Raycast(ev.Player.ReferenceHub.PlayerCameraReference.position + ev.Player.ReferenceHub.PlayerCameraReference.forward * 0.2f, ev.Player.ReferenceHub.PlayerCameraReference.forward, out RaycastHit hit, 10))
+            {
+                var transform = hit.transform;
+
+                if (transform.name.StartsWith("Input"))
+                {
+                    ev.IsAllowed = false;
+
+                    Transform sound = transform.parent.parent.GetChild(3).GetChild(2);
+
+                    if (ev.Player.CurrentItem != null && ev.Player.CurrentItem.Type == ItemType.Coin)
+                    {
+                        Tools.PlaySound(sound, "vm_insert_success", 2);
+
+                        ev.Player.RemoveItem(ev.Player.CurrentItem);
+
+                        Transform output = transform.parent.parent.GetChild(3).GetChild(0);
+
+                        if (ev.Player.IsScp)
+                        {
+                            yield return Timing.WaitForSeconds(0.5f);
+
+                            Tools.PlaySound(output, "vm_drop", 2);
+
+                            yield return Timing.WaitForSeconds(0.5f);
+
+                            List<EffectType> effects = new()
+                        {
+                            EffectType.MovementBoost,
+                            EffectType.Invisible,
+                            EffectType.AntiScp207,
+                            EffectType.Invigorated,
+                            EffectType.DamageReduction,
+                        };
+
+                            ev.Player.AddEffect
+                            (
+                                effects.GetRandomValue(),
+                                (byte)UnityEngine.Random.Range(1, UnityEngine.Random.Range(10, 256)),
+                                UnityEngine.Random.Range(1, UnityEngine.Random.Range(10, 61)),
+                                true
+                            );
+                        }
+                        else
+                        {
+                            List<ItemType> list = new();
+
+                            foreach (var chance in vandingMachineChances)
+                            {
+                                for (int i = 0; i < chance.Value; i++)
+                                {
+                                    list.Add(chance.Key);
+                                }
+                            }
+
+                            yield return Timing.WaitForSeconds(0.5f);
+
+                            Tools.PlaySound(output, "vm_drop", 2);
+
+                            yield return Timing.WaitForSeconds(0.5f);
+
+                            ItemType itemType = list.GetRandomValue();
+                            if (itemType == ItemType.SCP330)
+                            {
+                                Scp330 scp330 = (Scp330)Item.Create(ItemType.SCP330);
+                                scp330.AddCandy(Tools.EnumToList<CandyKindID>().GetRandomValue());
+                                scp330.RemoveCandy(scp330.Candies.ToList()[0]);
+
+                                scp330.CreatePickup(output.position);
+                            }
+                            else
+                            {
+                                Pickup.CreateAndSpawn(itemType, output.position);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Tools.PlaySound(sound, "vm_insert_fail", 2);
+                    }
+                }
+            }
+        }
+
+        public void OnDying(DyingEventArgs ev)
         {
             if (ev.Attacker != null)
             {
-                ev.Attacker.AddItem(ItemType.Coin);
+                ev.Attacker.AddItem(ItemType.Coin, ev.Player.IsScp ? 5 : 1);
             }
         }
     }
