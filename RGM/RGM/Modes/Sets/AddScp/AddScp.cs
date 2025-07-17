58ea4f8d11882bb42681acd079dadfb09775fe03
GoldenPig1205 <pkj7354@gmail.com>
2025-07-17 17:25:25 +0900

    추가 SCPㅣ모든 SCP의 확률제를 폐지합니다.

--- RGM/RGM/Modes/Sets/AddScp/AddScp.cs
+++ RGM/RGM/Modes/Sets/AddScp/AddScp.cs
@@ -29,13 +29,13 @@ namespace RGM.Modes
 """
 새로운 SCP..?
 
-<color=red>SCP-008</color> : 좀비 전염병 (10+, 인간, 1/2)
-<color=red>SCP-035</color> : 빙의 가면 (10+, 인간, 1/2)
-<color=red>SCP-294</color> : 자판기 (2/3)
-<color=red>SCP-457</color> : 불타는 사람 (15+, SCP, 1/2)
-<color=red>SCP-966</color> : 잠을 죽이는 자 (15+, SCP, 1/2)
-<color=red>SCP-999</color> : 간지럼 괴물 (5+, 인간, 1/2)
-<color=red>SCP-1162</color> : 벽 속의 구멍 (2/3)
+<color=red>SCP-008</color> : 좀비 전염병 (10+)
+<color=red>SCP-035</color> : 빙의 가면 (10+)
+<color=red>SCP-294</color> : 자판기
+<color=red>SCP-457</color> : 불타는 사람 (15+)
+<color=red>SCP-966</color> : 잠을 죽이는 자 (15+)
+<color=red>SCP-999</color> : 간지럼 괴물 (5+)
+<color=red>SCP-1162</color> : 벽 속의 구멍
 """;
         public override string Color => "fd0101";
 
@@ -52,65 +52,47 @@ namespace RGM.Modes
 
         public IEnumerator<float> OnModeStarted()
         {
-            if (Random.Range(1, 4) > 1)
-                Scp294.OnEnabled();
-
-            if (Random.Range(1, 4) > 1)
-                Scp1162.OnEnabled();
+            Scp294.OnEnabled();
+            Scp1162.OnEnabled();
 
             if (Server.PlayerCount >= 5)
             {
-                if (Random.Range(1, 3) == 1)
-                {
-                    Player scp999 = Player.List.GetRandomValue(x => !x.IsScp && x.IsAlive && !specialScps.Contains(x));
-                    specialScps.Add(scp999);
+                Player scp999 = Player.List.GetRandomValue(x => !x.IsScp && x.IsAlive && !specialScps.Contains(x));
+                specialScps.Add(scp999);
 
-                    Door door = Door.Get(DoorType.HIDLab);
-                    scp999.Position = door.Position + new Vector3(0, 2, 0);
+                Door door = Door.Get(DoorType.HIDLab);
+                scp999.Position = door.Position + new Vector3(0, 2, 0);
 
-                    Scp999.Create(scp999);
-                }
+                Scp999.Create(scp999);
             }
 
             if (Server.PlayerCount >= 10)
             {
-                if (Random.Range(1, 3) == 1)
-                {
-                    Player scp008 = Player.List.GetRandomValue(x => !x.IsScp && x.IsAlive && !specialScps.Contains(x));
-                    specialScps.Add(scp008);
+                Player scp008 = Player.List.GetRandomValue(x => !x.IsScp && x.IsAlive && !specialScps.Contains(x));
+                specialScps.Add(scp008);
 
-                    Scp008.Create(scp008);
-                }
+                Scp008.Create(scp008);
 
-                if (Random.Range(1, 3) == 1)
-                {
-                    Player scp035 = Player.List.GetRandomValue(x => !x.IsScp && x.IsAlive && !specialScps.Contains(x));
-                    specialScps.Add(scp035);
+                Player scp035 = Player.List.GetRandomValue(x => !x.IsScp && x.IsAlive && !specialScps.Contains(x));
+                specialScps.Add(scp035);
 
-                    Door door = Door.Get(DoorType.ElevatorNuke);
-                    scp035.Position = door.Position + new Vector3(0, 2, 0);
+                Door door = Door.Get(DoorType.ElevatorNuke);
+                scp035.Position = door.Position + new Vector3(0, 2, 0);
 
-                    Scp035.Create(scp035);
-                }
+                Scp035.Create(scp035);
             }
 
             if (Server.PlayerCount >= 15)
             {
-                if (Random.Range(1, 3) == 1)
-                {
-                    Player scp457 = Player.List.GetRandomValue(x => x.IsScp && x.IsAlive && !specialScps.Contains(x));
-                    specialScps.Add(scp457);
+                Player scp457 = Player.List.GetRandomValue(x => x.IsScp && x.IsAlive && !specialScps.Contains(x));
+                specialScps.Add(scp457);
 
-                    Scp457.Create(scp457);
-                }
+                Scp457.Create(scp457);
 
-                if (Random.Range(1, 3) == 1)
-                {
-                    Player scp966 = Player.List.GetRandomValue(x => x.IsScp && x.IsAlive && !specialScps.Contains(x));
-                    specialScps.Add(scp966);
+                Player scp966 = Player.List.GetRandomValue(x => x.IsScp && x.IsAlive && !specialScps.Contains(x));
+                specialScps.Add(scp966);
 
-                    Scp966.Create(scp966);
-                }
+                Scp966.Create(scp966);
             }
 
             yield break;
