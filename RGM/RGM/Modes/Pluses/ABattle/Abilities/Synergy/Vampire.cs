8d8728e3b761e6f1ffc1f97d8188aa5d62c35066
GoldenPig1205 <pkj7354@gmail.com>
2025-07-17 17:24:42 +0900

    워업ㅣ"뱀파이어" 능력이 작동되지 않는 버그를 수정하기 위해 노력함

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Synergy/Vampire.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Synergy/Vampire.cs
@@ -13,7 +13,7 @@ using UnityEngine;
 namespace RGM.Modes.Abilities.Synergy;
 
 [RequiresAbility(AbilityType.RARE_VAMPIRE, AbilityType.RARE_TRANSPARENTCLOAK)]
-[Ability("뱀파이어", "<흡혈귀, 투명 망토> 마침 밤이군요, 활동할 시간입니다. 피해를 입히면 피해량의 6%를 즉시 회복합니다. <b>이 능력은 최대 체력을 무시합니다.</b>", AbilityCategory.Synergy, AbilityType.SYNERGY_VAMPIRE)]
+[Ability("뱀파이어", "<흡혈귀, 투명 망토> 마침 밤이군요, 활동할 시간입니다. 피해를 입히면 피해량의 4%를 즉시 회복합니다. <b>이 능력은 최대 체력을 무시합니다.</b>", AbilityCategory.Synergy, AbilityType.SYNERGY_VAMPIRE)]
 public class Vampire : Ability
 {
     public override void OnEnabled()
@@ -31,6 +31,6 @@ public class Vampire : Ability
         if (ev.Attacker == null || ev.Attacker != Owner || !HitboxIdentity.IsEnemy(ev.Attacker.ReferenceHub, ev.Player.ReferenceHub))
             return;
 
-        ev.Player.Health += (6 * ev.DamageHandler.Damage / 100);
+        ev.Attacker.Health += ev.DamageHandler.Damage / 25;
     }
 }
