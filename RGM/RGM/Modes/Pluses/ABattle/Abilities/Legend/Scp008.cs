e66f4852c53331530898c85fc7f94be89c16a331
GoldenPig1205 <pkj7354@gmail.com>
2025-07-19 22:42:32 +0900

    워업ㅣ추가 SCP의 SCP들을 전설 능력으로 만나보세요

---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Legend/Scp008.cs
@@ -0,0 +1,16 @@
+﻿using RGM.Modes.Sets.AddScp.Scps;
+
+namespace RGM.Modes.Abilities.Legend;
+
+[Ability("SCP-008, 좀비 전염병", "SCP-008로 변경됩니다.", AbilityCategory.Legend, AbilityType.LEGEND_SCP008)]
+public class ChangeScp008 : Ability
+{
+    public override void OnEnabled()
+    {
+        Scp008.Create(Owner);
+    }
+
+    public override void OnDisabled()
+    {
+    }
+}
---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Legend/Scp966.cs
@@ -0,0 +1,16 @@
+﻿using RGM.Modes.Sets.AddScp.Scps;
+
+namespace RGM.Modes.Abilities.Legend;
+
+[Ability("SCP-966, 불타는 남자", "SCP-966으로 변경됩니다.", AbilityCategory.Legend, AbilityType.LEGEND_SCP966)]
+public class ChangeScp966 : Ability
+{
+    public override void OnEnabled()
+    {
+        Scp966.Create(Owner);
+    }
+
+    public override void OnDisabled()
+    {
+    }
+}
