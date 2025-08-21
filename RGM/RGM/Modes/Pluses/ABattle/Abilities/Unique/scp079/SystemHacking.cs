2d04ba4f05bf765a5a34562ab4468eab6ca663b4
GoldenPig1205 <pkj7354@gmail.com>
2025-08-21 13:45:21 +0900

    워업ㅣSCP-079 전용 능력 "카메라 플래시"와 "시스템 해킹" 능력 추가

---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Unique/scp079/SystemHacking.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks.Sources;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.API.Features.Roles;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Scp079;
+using Exiled.Events.EventArgs.Scp106;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using PlayerRoles;
+using RGM.API.DataBases;
+using RGM.API.Features;
+using UnityEngine;
+
+namespace RGM.Modes.Abilities.Unique.Scp079;
+
+[Ability("시스템 해킹", "아군 SCP의 투명도를 2분 간 90% 증가시킵니다.", AbilityCategory.Scp079, AbilityType.SCP079_SYSTEMHACKING)]
+public class SystemHacking : Ability
+{
+    public override void OnEnabled()
+    {
+        foreach (var scp in Player.List.Where(x => x.IsScp && x.Role.Type != RoleTypeId.Scp079))
+        {
+            scp.AddEffect(EffectType.Fade, 90, 120);
+        }
+    }
+
+    public override void OnDisabled()
+    {
+    }
+}
