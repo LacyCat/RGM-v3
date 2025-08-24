65b00993d773419c2ff09c64d6b7f6db27e28b0d
GoldenPig1205 <pkj7354@gmail.com>
2025-08-24 18:19:10 +0900

    워업ㅣ"유기농 우유", "자동 방어 시스템" 능력 추가

---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Rare/OrganicMilk.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using RGM.API.Features;
+using UnityEngine;
+
+namespace RGM.Modes.Abilities.Rare;
+
+[Ability("유기농 우유", "자신에게 해로운 효과만 제거합니다.", AbilityCategory.Rare, AbilityType.RARE_ORGANICMILK)]
+public class OrganicMilk : Ability
+{
+    ushort CoinSerial = 0;
+
+    public override void OnEnabled()
+    {
+        Item item = Owner.AddItem(ItemType.Coin);
+        CoinSerial = item.Serial;
+
+        Exiled.Events.Handlers.Player.ChangedItem += OnChangedItem;
+        Exiled.Events.Handlers.Player.FlippingCoin += OnFlippingCoin;
+    }
+
+    public override void OnDisabled()
+    {
+    }
+
+    public void OnChangedItem(ChangedItemEventArgs ev)
+    {
+        if (ev.Item != null)
+        {
+            if (CoinSerial == ev.Item.Serial)
+                ev.Player.AddHint("동전 사용 설명", $"이 동전을 튕기면 <b><color={ABattle.RatingColor["희귀"]}>유기농 우유</color></b> 능력을 사용할 수 있습니다.");
+        }
+    }
+
+    public void OnFlippingCoin(FlippingCoinEventArgs ev)
+    {
+        if (CoinSerial == ev.Item.Serial && ev.Player.CurrentRoom.Type != RoomType.Pocket)
+        {
+            foreach (var effect in Owner.ActiveEffects)
+            {
+                if (effect.GetEffectType().IsNegative() || effect.GetEffectType().IsHarmful())
+                {
+                    Owner.DisableEffect(effect.GetEffectType());
+                }
+            }
+
+            ev.Item.Destroy();
+        }
+    }
+}
---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Unique/scp079/AutoTesla.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks.Sources;
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.API.Features.Roles;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Scp079;
+using Exiled.Events.EventArgs.Scp106;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using RGM.API.DataBases;
+using RGM.API.Features;
+using UnityEngine;
+
+namespace RGM.Modes.Abilities.Unique.Scp079;
+
+[Ability("자동 방어 시스템", "테슬라가 가끔씩 자동으로 작동됩니다.", AbilityCategory.Scp079, AbilityType.SCP079_AUTOTESLA)]
+public class AutoTesla : Ability
+{
+    CoroutineHandle _autoTesla;
+
+    public override void OnEnabled()
+    {
+        _autoTesla = Timing.RunCoroutine(autoTesla());
+    }
+
+    public override void OnDisabled()
+    {
+        Timing.KillCoroutines(_autoTesla);
+    }
+
+    IEnumerator<float> autoTesla()
+    {
+        while (true)
+        {
+            TeslaGate tesla = TeslaGate.AllGates.GetRandomValue();
+
+            tesla.UserCode_RpcInstantBurst();
+
+            yield return Timing.WaitForSeconds(UnityEngine.Random.Range(1, 120));
+        }
+    }
+}
