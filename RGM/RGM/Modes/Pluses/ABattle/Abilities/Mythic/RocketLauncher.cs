7b58f0198b143b6207955d739ccabe10119471bd
GoldenPig1205 <pkj7354@gmail.com>
2025-09-02 09:03:09 +0900

    워업ㅣ"로켓 런처" 데미지 버그 수정 <#1412074004025446400>

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Mythic/RocketLauncher.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Mythic/RocketLauncher.cs
@@ -5,6 +5,7 @@ using Exiled.Events.EventArgs.Player;
 using MEC;
 using RGM.API.DataBases;
 using RGM.API.Features;
+using System.Collections.Generic;
 using UnityEngine;
 using static RGM.Variables.ServerManagers;
 
@@ -13,6 +14,8 @@ namespace RGM.Modes.Abilities.Mythic;
 [Ability("로켓 런처", "공격 시, 5% 확률로 상대방을 하늘로 승천시킬 수 있습니다! (<color=red>SCP</color>는 20%)", AbilityCategory.Mythic, AbilityType.MYTHIC_ROCKETLAUNCHER)]
 public class RocketLauncher : Ability
 {
+    List<Player> isInRocket = new();
+
     public override void OnEnabled()
     {
         Exiled.Events.Handlers.Player.Hurting += OnHurting;
@@ -28,20 +31,37 @@ public class RocketLauncher : Ability
         if (ev.Attacker == null || ev.Attacker != Owner || !HitboxIdentity.IsEnemy(ev.Attacker.ReferenceHub, ev.Player.ReferenceHub))
             return;
 
-        if (ev.Attacker.IsScp)
+        if (!isInRocket.Contains(ev.Player))
         {
-            if (Random.Range(1, 6) == 1)
+            if (ev.Attacker.IsScp)
             {
-                Timing.RunCoroutine(Tools.DoRocket(Owner, ev.Player, 1));
-                Server.ExecuteCommand($"/cassie_sl {ev.Player.DisplayNickname}(<color={ev.Player.Role.Color.ToHex()}>{(en ? ev.Player.Role : Trans.Role[ev.Player.Role.Type])}</color>)(이)가 하늘로 승천했습니다.");
+                if (Random.Range(1, 6) == 1)
+                {
+                    isInRocket.Add(ev.Player);
+
+                    Timing.RunCoroutine(Tools.DoRocket(Owner, ev.Player, 1));
+                    Server.ExecuteCommand($"/cassie_sl {ev.Player.DisplayNickname}(<color={ev.Player.Role.Color.ToHex()}>{(en ? ev.Player.Role : Trans.Role[ev.Player.Role.Type])}</color>)(이)가 하늘로 승천했습니다.");
+
+                    Timing.CallDelayed(1, () =>
+                    {
+                        isInRocket.Remove(ev.Player);
+                    });
+                }
             }
-        }
-        else
-        {
-            if (Random.Range(1, 21) == 1)
+            else
             {
-                Timing.RunCoroutine(Tools.DoRocket(Owner, ev.Player, 1));
-                Server.ExecuteCommand($"/cassie_sl {ev.Player.DisplayNickname}(<color={ev.Player.Role.Color.ToHex()}>{(en ? ev.Player.Role : Trans.Role[ev.Player.Role.Type])}</color>)(이)가 하늘로 승천했습니다.");
+                if (Random.Range(1, 21) == 1)
+                {
+                    isInRocket.Add(ev.Player);
+
+                    Timing.RunCoroutine(Tools.DoRocket(Owner, ev.Player, 1));
+                    Server.ExecuteCommand($"/cassie_sl {ev.Player.DisplayNickname}(<color={ev.Player.Role.Color.ToHex()}>{(en ? ev.Player.Role : Trans.Role[ev.Player.Role.Type])}</color>)(이)가 하늘로 승천했습니다.");
+
+                    Timing.CallDelayed(1, () =>
+                    {
+                        isInRocket.Remove(ev.Player);
+                    });
+                }
             }
         }
     }
