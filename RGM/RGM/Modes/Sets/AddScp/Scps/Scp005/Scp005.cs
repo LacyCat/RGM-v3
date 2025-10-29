049263041bf721d084fe55e587a01a10267878ce
GoldenPig1205 <pkj7354@gmail.com>
2025-10-29 20:58:26 +0900

    추가 SCPㅣ"SCP-005"가 모든 문을 열 수 있던 버그 수정

--- RGM/RGM/Modes/Sets/AddScp/Scps/Scp005/Scp005.cs
+++ RGM/RGM/Modes/Sets/AddScp/Scps/Scp005/Scp005.cs
@@ -18,6 +18,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using static RGM.Variables.ServerManagers;
+using static UnityEngine.UI.GridLayoutGroup;
 
 namespace RGM.Modes.Sets.AddScp.Scps
 {
@@ -35,19 +36,11 @@ namespace RGM.Modes.Sets.AddScp.Scps
                     {
                         if (player.CurrentItem != null && player.CurrentItem.Serial == item.Serial)
                         {
-                            if (!player.IsBypassModeEnabled)
-                                player.IsBypassModeEnabled = true;
-
                             player.AddHint("SCP-005",
 """
 <size=25>이 동전은 <color=red>SCP-005</color>(<color=#a4fc16>Safe</color>)입니다.</size>
 <size=20>모든 것을 열 수 있습니다.</size>
 """, 0.12f);
-                        }
-                        else
-                        {
-                            if (player.IsBypassModeEnabled)
-                                player.IsBypassModeEnabled = false;
                         }
                     }
 
@@ -56,6 +49,71 @@ namespace RGM.Modes.Sets.AddScp.Scps
             }
 
             Timing.RunCoroutine(displayDesc());
+
+            void OnInteractingDoor(InteractingDoorEventArgs ev)
+            {
+                if (ev.Player.CurrentItem == item)
+                {
+                    if (ev.Door.Type == DoorType.Scp079First)
+                    {
+                        ev.Player.AddHint("헤비도어", "이 헤비도어는 능력으로 개폐가 불가능합니다.", 1.2f);
+                        return;
+                    }
+
+                    if (Warhead.IsInProgress)
+                    {
+                        ev.Player.AddHint("알파 핵탄투", "알파 핵탄투가 작동 중일때는 문을 개폐할 수 없습니다.", 1.2f);
+                        return;
+                    }
+
+                    ev.IsAllowed = false;
+
+                    if (ev.Door.IsOpen)
+                        ev.Door.IsOpen = false;
+
+                    else
+                        ev.Door.IsOpen = true;
+                }
+            }
+
+            void OnInteractingLocker(InteractingLockerEventArgs ev)
+            {
+                if (ev.Player.CurrentItem == item)
+                {
+                    ev.IsAllowed = false;
+
+                    if (ev.InteractingChamber.IsOpen)
+                        ev.InteractingChamber.IsOpen = false;
+
+                    else
+                        ev.InteractingChamber.IsOpen = true;
+                }
+            }
+
+            void OnOpeningGenerator(OpeningGeneratorEventArgs ev)
+            {
+                if (ev.Player.CurrentItem == item)
+                {
+                    ev.IsAllowed = false;
+
+                    ev.Generator.IsOpen = false;
+                }
+            }
+
+            void OnClosingGenerator(ClosingGeneratorEventArgs ev)
+            {
+                if (ev.Player.CurrentItem == item)
+                {
+                    ev.IsAllowed = false;
+
+                    ev.Generator.IsOpen = true;
+                }
+            }
+
+            Exiled.Events.Handlers.Player.InteractingDoor += OnInteractingDoor;
+            Exiled.Events.Handlers.Player.InteractingLocker += OnInteractingLocker;
+            Exiled.Events.Handlers.Player.OpeningGenerator += OnOpeningGenerator;
+            Exiled.Events.Handlers.Player.ClosingGenerator += OnClosingGenerator;
 
             return item;
         }
