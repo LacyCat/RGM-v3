5a60af17f7b210b48038e8d24aef3328f2c3dec9
GoldenPig1205 <pkj7354@gmail.com>
2025-11-15 11:41:23 +0900

    할로윈 관련 이스터에그 확률 조정 롤백

--- RGM/RGM/EventArgs/ServerEvents.cs
+++ RGM/RGM/EventArgs/ServerEvents.cs
@@ -212,7 +212,7 @@ namespace RGM.EventArgs
                     Timing.RunCoroutine(HumanLoop());
                     Timing.RunCoroutine(Scp079Broadcast());
 
-                    int num = UnityEngine.Random.Range(1, 11); //할로윈 이벤트 <- UnityEngine.Random.Range(1, 51);
+                    int num = UnityEngine.Random.Range(1, 51);
 
                     if (num == 1)
                     {
@@ -224,7 +224,7 @@ namespace RGM.EventArgs
                             }
                         }
 
-                        if (UnityEngine.Random.Range(1, 2) == 1) //할로윈 이벤트 <- (UnityEngine.Random.Range(1, 3) == 1)
+                        if (UnityEngine.Random.Range(1, 3) == 1)
                             Scp294.OnEnabled();
 
                         if (UnityEngine.Random.Range(1, 3) == 1)
