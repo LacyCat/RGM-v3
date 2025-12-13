69d2c7a2d40ca500271067d344428dc65886ae3f
GoldenPig1205 <pkj7354@gmail.com>
2025-12-13 17:04:17 +0900

    지원은 이제 훈련장의 플레이어와 오버워치 역할들을 징집시킵니다

---
+++ RGM/RGM/Patches/Broadcast.cs
@@ -0,0 +1,16 @@
+﻿using Exiled.API.Features;
+using MultiBroadcast.API;
+
+namespace RGM.Patches
+{
+    public class BroadcastPostfix
+    {
+        public static void Postfix(ushort duration, string message, Broadcast.BroadcastFlags type, bool shouldClearPrevious)
+        {
+            foreach (var player in Player.List)
+            {
+                player.AddBroadcast(duration, message);
+            }
+        }
+    }
+}
---
+++ RGM/RGM/Patches/Wave.cs
@@ -0,0 +1,16 @@
+﻿using Exiled.API.Features;
+using MultiBroadcast.API;
+using static RGM.Variables.ServerManagers;
+
+namespace RGM.Patches
+{
+    public class WavePostfix
+    {
+        public static void Postfix(ReferenceHub player, ref bool __result)
+        {
+            Player ply = Player.Get(player);
+
+            __result = ply.IsDead || NonePlayers.Contains(ply);
+        }
+    }
+}
