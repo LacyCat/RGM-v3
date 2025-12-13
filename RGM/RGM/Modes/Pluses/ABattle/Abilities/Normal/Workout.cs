b923e75db5d9149be1caff880894970a494d6438
GoldenPig1205 <pkj7354@gmail.com>
2025-12-13 17:04:48 +0900

    워업ㅣ"운동" 능력 변경 (25% -> 25 (SCP는 250))

--- RGM/RGM/Modes/Pluses/ABattle/Abilities/Normal/Workout.cs
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Normal/Workout.cs
@@ -1,13 +1,13 @@
 ﻿namespace RGM.Modes.Abilities.Normal;
 
-[Ability("운동", "25%만큼 최대 체력을 추가합니다.", AbilityCategory.Common, AbilityType.NORMAL_WORKOUT)]
+[Ability("운동", "25만큼 최대 체력을 추가합니다. (SCP는 10배의 보너스를 받습니다.)", AbilityCategory.Common, AbilityType.NORMAL_WORKOUT)]
 public class Workout : Ability
 {
     private float _additionHealth;
 
     public override void OnEnabled()
     {
-        _additionHealth = Owner.MaxHealth * 0.25f;
+        _additionHealth = Owner.IsScp ? 250 : 25;
         Owner.MaxHealth += _additionHealth;
         Owner.Health += _additionHealth;
     }
