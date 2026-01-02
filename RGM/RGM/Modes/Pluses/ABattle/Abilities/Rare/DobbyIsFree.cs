b44f4b4a11e1a86b20c65c9220d1f26a6a1948bd
GoldenPig1205 <pkj7354@gmail.com>
2026-01-02 16:37:39 +0900

    워업ㅣ크리스마스 전용 능력 하나 추가

---
+++ RGM/RGM/Modes/Pluses/ABattle/Abilities/Rare/DobbyIsFree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using RGM.API.Features;
+using UnityEngine;
+
+namespace RGM.Modes.Abilities.Rare;
+
+[Ability("도비는 자유에요", "석탄?을 지급받습니다.", AbilityCategory.Rare, AbilityType.RARE_DOBBYISFREE, AbilityHolidayType.Christmas)]
+public class DobbyIsFree : Ability
+{
+    public override void OnEnabled()
+    {
+        Owner.AddItem(ItemType.SpecialCoal);
+    }
+
+    public override void OnDisabled()
+    {
+    }
+}
