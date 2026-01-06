5e548b2b1a799e19f3cf5ac8f44f1d1c60cead88
GoldenPig1205 <pkj7354@gmail.com>
2026-01-06 22:55:52 +0900

    랜덤게임모드> v3.20.13 릴리즈

--- RGM/Plugin.cs
+++ RGM/Plugin.cs
@@ -41,7 +41,7 @@ namespace RGM
 
         public override string Name => "RGM";
         public override string Author => "GoldenPig1205";
-        public override Version Version { get; } = new(3, 20, 12);
+        public override Version Version { get; } = new(3, 20, 13);
         public override Version RequiredExiledVersion { get; } = new(1, 2, 0, 5);
 
         public override void OnEnabled()
@@ -112,7 +112,6 @@ namespace RGM
             Exiled.Events.Handlers.Player.VoiceChatting += OnVoiceChatting;
             Exiled.Events.Handlers.Player.DamagingShootingTarget += OnDamagingShootingTarget;
 
-            Exiled.Events.Handlers.Warhead.Stopping += OnStopping;
             Exiled.Events.Handlers.Warhead.Detonating += OnDetonating;
 
             Exiled.Events.Handlers.Scp1509.Resurrecting += OnResurrecting;
