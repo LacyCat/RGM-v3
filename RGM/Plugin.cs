af0bf1b53dfb362c4290436f3179c7fb936461be
GoldenPig1205 <pkj7354@gmail.com>
2026-03-13 00:02:28 +0900

    랜덤게임모드> v3.21.6 릴리즈

--- RGM/Plugin.cs
+++ RGM/Plugin.cs
@@ -1,31 +1,15 @@
-﻿using Exiled.API.Enums;
-using Exiled.API.Extensions;
-using Exiled.API.Features;
+﻿using Exiled.API.Features;
 using HarmonyLib;
 using MapGeneration.Holidays;
-using MEC;
-
-using ProjectMER.Features.Objects;
-using Respawning;
 using Respawning.Waves;
-using RGM.API.Components;
-using RGM.API.DataBases;
 using RGM.API.Features;
-using RGM.API.Interfaces;
-using RGM.Modes;
 using RGM.Patches;
 using RGM.UserSettings;
-using RGM.Variables;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
 using UserSettings.ServerSpecific;
-using YamlDotNet.Core.Tokens;
 using static RGM.EventArgs.MEREvents;
 using static RGM.EventArgs.PlayerEvents;
 using static RGM.EventArgs.Scp079Events;
@@ -35,6 +19,7 @@ using static RGM.EventArgs.Scp330Events;
 using static RGM.EventArgs.ServerEvents;
 using static RGM.EventArgs.WarheadEvents;
 using static RGM.Variables.Variable;
+using Exiled.Events.EventArgs.Player;
 
 namespace RGM
 {
@@ -44,7 +29,7 @@ namespace RGM
 
         public override string Name => "RGM";
         public override string Author => "GoldenPig1205";
-        public override Version Version { get; } = new(3, 21, 5);
+        public override Version Version { get; } = new(3, 21, 6);
         public override Version RequiredExiledVersion { get; } = new(1, 2, 0, 5);
 
         public override void OnEnabled()
@@ -99,73 +84,97 @@ namespace RGM
 
             // ------------------------------------------------------------------------------------------------------
 
-            Exiled.Events.Handlers.Server.WaitingForPlayers += OnWaitingForPlayers;
-            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
-            Exiled.Events.Handlers.Server.RoundEnded += OnRoundEnded;
-            Exiled.Events.Handlers.Server.RespawnedTeam += OnRespawnedTeam;
+            if (Instance.Config.FixedMode == ModeType.None)
+            {
+                Exiled.Events.Handlers.Server.WaitingForPlayers += OnWaitingForPlayers;
+                Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+                Exiled.Events.Handlers.Server.RoundEnded += OnRoundEnded;
+                Exiled.Events.Handlers.Server.RespawnedTeam += OnRespawnedTeam;
 
-            Exiled.Events.Handlers.Player.Verified += OnVerified;
-            Exiled.Events.Handlers.Player.Left += OnLeft;
-            Exiled.Events.Handlers.Player.SpawningRagdoll += OnSpawningRagdoll;
-            Exiled.Events.Handlers.Player.SpawnedRagdoll += OnSpawnedRagdoll;
-            Exiled.Events.Handlers.Player.ChangingRole += OnChangingRole;
-            Exiled.Events.Handlers.Player.Spawned += OnSpawned;
-            Exiled.Events.Handlers.Player.InteractingDoor += OnInteractingDoor;
-            Exiled.Events.Handlers.Player.Hurting += OnHurting;
-            Exiled.Events.Handlers.Player.Dying += OnDying;
-            Exiled.Events.Handlers.Player.Died += OnDied;
-            Exiled.Events.Handlers.Player.ItemAdded += OnItemAdded;
-            Exiled.Events.Handlers.Player.UsingItem += OnUsingItem;
-            Exiled.Events.Handlers.Player.DroppingItem += OnDroppingItem;
-            Exiled.Events.Handlers.Player.DroppingAmmo += OnDroppingAmmo;
-            Exiled.Events.Handlers.Player.DroppedItem += OnDroppedItem;
-            Exiled.Events.Handlers.Player.DroppedAmmo += OnDroppedAmmo;
-            Exiled.Events.Handlers.Player.Shooting += OnShooting;
-            Exiled.Events.Handlers.Player.Kicking += OnKicking;
-            Exiled.Events.Handlers.Player.Banning += OnBanning;
-            Exiled.Events.Handlers.Player.ChangingGroup += OnChangingGroup;
-            Exiled.Events.Handlers.Player.VoiceChatting += OnVoiceChatting;
-            Exiled.Events.Handlers.Player.DamagingShootingTarget += OnDamagingShootingTarget;
+                Exiled.Events.Handlers.Player.Verified += OnVerified;
+                Exiled.Events.Handlers.Player.Left += OnLeft;
+                Exiled.Events.Handlers.Player.SpawningRagdoll += OnSpawningRagdoll;
+                Exiled.Events.Handlers.Player.SpawnedRagdoll += OnSpawnedRagdoll;
+                Exiled.Events.Handlers.Player.ChangingRole += OnChangingRole;
+                Exiled.Events.Handlers.Player.Spawned += OnSpawned;
+                Exiled.Events.Handlers.Player.InteractingDoor += OnInteractingDoor;
+                Exiled.Events.Handlers.Player.Hurting += OnHurting;
+                Exiled.Events.Handlers.Player.Dying += OnDying;
+                Exiled.Events.Handlers.Player.Died += OnDied;
+                Exiled.Events.Handlers.Player.ItemAdded += OnItemAdded;
+                Exiled.Events.Handlers.Player.UsingItem += OnUsingItem;
+                Exiled.Events.Handlers.Player.DroppingItem += OnDroppingItem;
+                Exiled.Events.Handlers.Player.DroppingAmmo += OnDroppingAmmo;
+                Exiled.Events.Handlers.Player.DroppedItem += OnDroppedItem;
+                Exiled.Events.Handlers.Player.DroppedAmmo += OnDroppedAmmo;
+                Exiled.Events.Handlers.Player.Shooting += OnShooting;
+                Exiled.Events.Handlers.Player.Kicking += OnKicking;
+                Exiled.Events.Handlers.Player.Banning += OnBanning;
+                Exiled.Events.Handlers.Player.ChangingGroup += OnChangingGroup;
+                Exiled.Events.Handlers.Player.VoiceChatting += OnVoiceChatting;
+                Exiled.Events.Handlers.Player.DamagingShootingTarget += OnDamagingShootingTarget;
 
-            Exiled.Events.Handlers.Warhead.Detonating += OnDetonating;
+                Exiled.Events.Handlers.Warhead.Detonating += OnDetonating;
 
-            Exiled.Events.Handlers.Scp1509.Resurrecting += OnResurrecting;
+                Exiled.Events.Handlers.Scp1509.Resurrecting += OnResurrecting;
 
-            Exiled.Events.Handlers.Scp330.InteractingScp330 += OnInteractingScp330;
-            Exiled.Events.Handlers.Scp330.EatingScp330 += OnEatingScp330;
+                Exiled.Events.Handlers.Scp330.InteractingScp330 += OnInteractingScp330;
+                Exiled.Events.Handlers.Scp330.EatingScp330 += OnEatingScp330;
 
-            Exiled.Events.Handlers.Scp244.UsingScp244 += OnUsingScp244;
-            Exiled.Events.Handlers.Scp244.OpeningScp244 += OnOpeningScp244;
+                Exiled.Events.Handlers.Scp244.UsingScp244 += OnUsingScp244;
+                Exiled.Events.Handlers.Scp244.OpeningScp244 += OnOpeningScp244;
 
-            Exiled.Events.Handlers.Scp079.Recontained += OnRecontained;
+                Exiled.Events.Handlers.Scp079.Recontained += OnRecontained;
 
-            ProjectMER.Events.Handlers.Schematic.SchematicSpawned += OnSchematicSpawned;
-            ProjectMER.Events.Handlers.Schematic.SchematicDestroyed += OnSchematicDestroyed;
+                ProjectMER.Events.Handlers.Schematic.SchematicSpawned += OnSchematicSpawned;
+                ProjectMER.Events.Handlers.Schematic.SchematicDestroyed += OnSchematicDestroyed;
 
-            // ------------------------------------------------------------------------------------------------------
+                // ------------------------------------------------------------------------------------------------------
 
-            ServerSpecificSettings.Init();
+                ServerSpecificSettings.Init();
 
-            ServerSpecificSettingsSync.ServerOnSettingValueReceived += ServerSpecificSettings.OnSSInput;
+                ServerSpecificSettingsSync.ServerOnSettingValueReceived += ServerSpecificSettings.OnSSInput;
 
-            // ------------------------------------------------------------------------------------------------------
+                // ------------------------------------------------------------------------------------------------------
 
-            Harmony harmony = new Harmony($"Harmony - {DateTime.Now.Ticks}");
+                TranslationManager.ApiKey = Tools.ReadTextFile(Path.Combine(Paths.Configs, "RGM"), "GoogleAPIKey.txt");
+                TranslationManager.IsEnabled = true;
+                TranslationManager.Debug = false;
 
-            // Postfix
-            harmony.Patch(AccessTools.Method(typeof(Map), nameof(Map.Broadcast), [typeof(ushort), typeof(string), typeof(Broadcast.BroadcastFlags), typeof(bool)]),
-                postfix: new HarmonyMethod(AccessTools.Method(typeof(BroadcastPatch), nameof(BroadcastPatch.MapBroadcastPostfix))));
-            harmony.Patch(AccessTools.Method(typeof(WaveSpawner), nameof(WaveSpawner.CanBeSpawned), [typeof(ReferenceHub)]),
-                postfix: new HarmonyMethod(AccessTools.Method(typeof(WavePatch), nameof(WavePatch.Postfix))));
-
-            // ------------------------------------------------------------------------------------------------------
-
-            TranslationManager.ApiKey = Tools.ReadTextFile(Path.Combine(Paths.Configs, "RGM"), "GoogleAPIKey.txt");
-            TranslationManager.IsEnabled = true;
-            TranslationManager.Debug = false;
-
-            if (Server.Port == 7803)
                 TranslationManager.StartWorker();
+
+                // ------------------------------------------------------------------------------------------------------
+
+                Harmony harmony = new Harmony($"Harmony - {DateTime.Now.Ticks}");
+
+                // Postfix
+                harmony.Patch(AccessTools.Method(typeof(Map), nameof(Map.Broadcast), [typeof(ushort), typeof(string), typeof(Broadcast.BroadcastFlags), typeof(bool)]),
+                    postfix: new HarmonyMethod(AccessTools.Method(typeof(BroadcastPatch), nameof(BroadcastPatch.MapBroadcastPostfix))));
+                harmony.Patch(AccessTools.Method(typeof(WaveSpawner), nameof(WaveSpawner.CanBeSpawned), [typeof(ReferenceHub)]),
+                    postfix: new HarmonyMethod(AccessTools.Method(typeof(WavePatch), nameof(WavePatch.Postfix))));
+            }
+            else
+            {
+                Exiled.Events.Handlers.Server.WaitingForPlayers += OnFixedModeWaitingForPlayers;
+                Exiled.Events.Handlers.Server.RoundStarted += OnFixedModeRoundStarted;
+
+                Exiled.Events.Handlers.Player.Verified += OnFixedModeVerified;
+            }
+        }
+
+        public static void OnFixedModeWaitingForPlayers()
+        {
+            ServerManager.Setup();
+        }
+
+        public static void OnFixedModeRoundStarted()
+        {
+            Tools.TryInstallMode(Instance.Config.FixedMode);
+        }
+
+        public static void OnFixedModeVerified(VerifiedEventArgs ev)
+        {
+            ev.Player.Setup();
         }
     }
 }
