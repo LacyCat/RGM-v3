f29903a5443c1fb4c18d4c10e0d8d544c441c805
GoldenPig1205 <78902671+GoldenPig1205@users.noreply.github.com>
2026-04-11 17:53:23 +0900

    관리자용 좌표 확인 명령어 추가

---
+++ RGM/RGM/Commands/RemoteAdminCommands/AdminFeatures/PrintPos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using CommandSystem;
+using Exiled.API.Features;
+using RGM.API.Features;
+
+namespace RGM.Commands.RemoteAdminCommands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class PrintPos : ICommand
+    {
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(sender);
+            string info = $"new Vector3({player.Position.x}, {player.Position.y}, {player.Position.z})";
+            Log.Info(info);
+
+            response = $"현재 위치: {info}";
+            return true;
+        }
+
+        public string Command { get; } = "printpos";
+
+        public string[] Aliases { get; } = { "print" };
+
+        public string Description { get; } = "현재의 위치 정보를 콘솔에 출력합니다.";
+
+        public bool SanitizeResponse { get; } = true;
+    }
+}
