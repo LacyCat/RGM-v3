38465147647002b8b2a1fa77431e91a61b634850
babycatoffical <wjsgusals32@gmail.com>
2026-06-03 14:18:13 +0900

    feat. Add new harmony feature: Scp1853 Patch for MaxIntensityPostfix
    Signed-off-by: babycatoffical <wjsgusals32@gmail.com>

---
+++ RGM/RGM/Modes/Plus/KoreanSpeed/Patches/SystemPatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Exiled.API.Features;
+
+namespace RGM.Modes.Patches;
+
+public static class SystemPatch
+{
+    public static void Scp1853MaxIntensityPostfix(ref byte __result)
+    {
+        try
+        {
+            __result = 100;
+        }
+        catch (Exception e)
+        {
+            Log.Error($"[KoreanSpeed/Scp1853MaxIntensityPostfix] Exception: {e}");
+        }
+    }
+}
