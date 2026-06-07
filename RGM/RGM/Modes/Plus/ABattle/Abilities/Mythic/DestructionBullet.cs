58b3d1bcd95f69f388ada6496c29b53503b0b012
FlyingRagdoll <FlyingRagd01l01@gmail.com>
2026-06-07 16:19:24 +0900

    Add files via upload

---
+++ RGM/RGM/Modes/Plus/ABattle/Abilities/Mythic/DestructionBullet.cs
@@ -0,0 +1,101 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using MEC;
+using RGM.API.Features;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RGM.Modes.Abilities.Mythic;
+
+[Ability("파괴 탄환", "10초마다 탄약이 하나 추가되고, 벽을 관통하고 대상의 능력을 90% 확률로 삭제하는 입자 분열기를 받습니다. 대상의 능력이 없으면 최대체력의 5배 피해를 입힙니다. 투시 능력을 얻습니다. (사거리 75)", AbilityCategory.Mythic, AbilityType.MYTHIC_DESTRUCTIONBULLET)]
+public class DestructionBullet : Ability
+{
+    ushort serial = 0;
+
+    public override void OnEnabled()
+    {
+        Owner.AddAbility(AbilityType.EPIC_SCP1344);
+
+        Item item = Owner.AddItem(ItemType.ParticleDisruptor);
+        serial = item.Serial;
+
+        Exiled.Events.Handlers.Player.ChangedItem += OnChangedItem;
+        Exiled.Events.Handlers.Player.Shot += OnShot;
+
+        Timing.RunCoroutine(PlanBAmmo());
+    }
+
+    public override void OnDisabled()
+    {
+    }
+
+    public IEnumerator<float> PlanBAmmo()
+    {
+        while (true)
+        {
+            yield return Timing.WaitForSeconds(10f);
+
+            if (Owner != null || Owner.IsAlive || Owner.CurrentItem != null)
+            {
+                Firearm firearm = (Firearm)Item.Get(serial);
+
+                if (firearm.MaxMagazineAmmo > firearm.MagazineAmmo)
+                    firearm.MagazineAmmo += 1;
+            }
+        }
+    }
+
+    public void OnChangedItem(ChangedItemEventArgs ev)
+    {
+        if (ev.Item != null && serial == ev.Player.CurrentItem.Serial)
+        {
+            if (serial == ev.Item.Serial)
+                ev.Player.AddHint("파괴 탄환", $"<b><color={ABattle.RatingColor["신화"]}>파괴 탄환</color></b> 능력이 있는 <b>입자 분열기</b>입니다!");
+        }
+    }
+
+    public void OnShot(ShotEventArgs ev)
+    {
+        if (serial == ev.Item.Serial)
+        {
+            if (Tools.TryGetLookPlayers(ev.Player, 75f, out List<Player> players, out RaycastHit? hit))
+            {
+                bool enemy = false;
+
+                foreach (var player in players)
+                {
+                    if (HitboxIdentity.IsEnemy(ev.Player.ReferenceHub, player.ReferenceHub))
+                    {
+                        if (ABattle.Instance.PlayerAbilities[player].Count <= 0 || !ABattle.Instance.PlayerAbilities.ContainsKey(player))
+                            player.Hit(ev.Player, player.MaxHealth * 5);
+                        //왜 그런진 모르겠지만 더미에게 쐈을 때 이 만큼의 데미지가 안 들어갑니다. 뭐지? 머리 아파서 워크 코드 전부 분석 안하긴 했는데
+
+                        else
+                        {
+                            if (Random.Range(1, 11) <= 9)
+                            {
+                                player.DisableAllEffects();
+                                player.RemoveAllAbilities();
+
+                                ABattle.Instance.PlayerWorkstations[player].Clear();
+                                ABattle.Instance.PlayerAbilities[player].Clear();
+                            }
+                        }
+                        
+                        
+                        enemy = true;
+                    }
+                }
+
+                if (!enemy)
+                {
+                }
+                else
+                {
+                    Hitmarker.SendHitmarkerDirectly(ev.Player.ReferenceHub, 3f);
+                }
+            }
+        }
+    }
+}
