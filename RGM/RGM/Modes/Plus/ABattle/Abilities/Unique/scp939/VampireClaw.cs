8400b6b06aba38065e56c016419c6d0bb27afdb5
Evan0717 <joungho0405@gmail.com>
2026-06-16 19:07:51 +0900

    1차 대규모 밸런스 패치 | 흡혈발톱 수정

--- RGM/RGM/Modes/Plus/ABattle/Abilities/Unique/scp939/VampireClaw.cs
+++ RGM/RGM/Modes/Plus/ABattle/Abilities/Unique/scp939/VampireClaw.cs
@@ -1,17 +1,24 @@
 ﻿using Exiled.API.Features.Roles;
+using Exiled.Events.EventArgs.Player;
 
 namespace RGM.Modes.Abilities.Unique.Scp939;
 
-[Ability("흡혈 발톱", "공격 시 36의 HS가 회복됩니다.", AbilityCategory.Scp939, AbilityType.SCP939_VAMPIRECLAW)]
+[Ability("흡혈 발톱", "공격 시 40의 HS가 회복됩니다.", AbilityCategory.Scp939, AbilityType.SCP939_VAMPIRECLAW)]
 public class VampireClaw : Ability
 {
     public override void OnEnabled()
     {
-        if (Owner.Role is Scp939Role Scp939)
-            Scp939.Owner.HumeShield += 36;
+        Exiled.Events.Handlers.Player.Hurting += OnHurting;
     }
 
     public override void OnDisabled()
     {
+        Exiled.Events.Handlers.Player.Hurting -= OnHurting;
+    }
+
+    public void OnHurting(HurtingEventArgs ev)
+    {
+        if (Owner.Role is Scp939Role Scp939)
+            Scp939.Owner.HumeShield += 40;
     }
 }
