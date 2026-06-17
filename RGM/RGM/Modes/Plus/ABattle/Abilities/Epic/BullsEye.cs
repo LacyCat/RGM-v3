26e020855e01335ab6fd348edb2b42406badbbcd
Evan0717 <joungho0405@gmail.com>
2026-06-17 22:14:33 +0900

    워크스테이션 업그레이드 | 능력 2개 추가

---
+++ RGM/RGM/Modes/Plus/ABattle/Abilities/Epic/BullsEye.cs
@@ -0,0 +1,28 @@
+using Exiled.API.Features.DamageHandlers;
+using Exiled.Events.EventArgs.Player;
+using LabApi.Events.Arguments.PlayerEvents;
+
+namespace RGM.Modes.Abilities.Epic;
+
+[Ability("불스아이", "헤드샷 데미지 배율이 60% 증가합니다.", AbilityCategory.Epic, AbilityType.EPIC_BULLSEYE)]
+public class BullsEye : Ability
+{
+    public override void OnEnabled()
+    {
+        Exiled.Events.Handlers.Player.Hurting += OnHurting;
+    }
+
+    public override void OnDisabled()
+    {
+        Exiled.Events.Handlers.Player.Hurting -= OnHurting;
+    }
+
+    public void OnHurting(HurtingEventArgs ev)
+    {
+        if (ev.Attacker != Owner || ev.DamageHandler.CustomBase is not FirearmDamageHandler damageHandler)
+            return;
+
+        if (damageHandler.Hitbox == HitboxType.Headshot)
+            damageHandler.Damage *= 1.6f;
+    }
+}
