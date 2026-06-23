aa2148b52d6e742da76daf044fdd349ba14e3013
babycatoffical <wjsgusals32@gmail.com>
2026-06-23 19:03:25 +0900

    feat. 핵탄두 관련 이벤트 핸들러 추가 및 보험/매드 사이언티스트 능력 로직 개선
    Signed-off-by: babycatoffical <wjsgusals32@gmail.com>

--- RGM/RGM/Modes/Plus/ABattle/Abilities/Epic/MadScientist.cs
+++ RGM/RGM/Modes/Plus/ABattle/Abilities/Epic/MadScientist.cs
@@ -1,48 +1,65 @@
 ﻿using System;
 using Exiled.API.Features;
 using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Warhead;
 using MEC;
 using PlayerRoles;
 using RGM.API.DataBases;
 
 namespace RGM.Modes.Abilities.Epic;
 
-[Ability("매드 사이언티스트", "사망 시, 10초 뒤에 해당 자리에서 리셋됩니다. 랜덤한 능력 5개가 지급됩니다.", AbilityCategory.Epic, AbilityType.EPIC_MADSCIENTIST)]
+[Ability("매드 사이언티스트", "사망 시, 10초 뒤에 해당 자리에서 리셋됩니다. 랜덤한 능력 5개가 지급됩니다.", AbilityCategory.Epic,
+    AbilityType.EPIC_MADSCIENTIST)]
 public class MadScientist : Ability
 {
+    private static bool _isDetonatingState;
+
     public override void OnEnabled()
     {
         Exiled.Events.Handlers.Player.Died += OnDied;
+        Exiled.Events.Handlers.Warhead.Detonating += OnDetonating;
     }
 
     public override void OnDisabled()
     {
         Exiled.Events.Handlers.Player.Died -= OnDied;
+        Exiled.Events.Handlers.Warhead.Detonating -= OnDetonating;
     }
 
-    public void OnDied(DiedEventArgs ev)
+    private void OnDied(DiedEventArgs ev)
     {
-        if (ev.Player != Owner || Datas.BlockDamageTypes.Contains(ev.DamageHandler.Type))
-            return;
-
-        Timing.CallDelayed(10, () =>
+        Timing.CallDelayed(Timing.WaitForOneFrame, () =>
         {
-            Owner.Role.Set(ev.TargetOldRole, RoleSpawnFlags.None);
+            if (ev.Player != Owner || Datas.BlockDamageTypes.Contains(ev.DamageHandler.Type) || _isDetonatingState)
+                return;
 
-            Timing.CallDelayed(Timing.WaitForOneFrame, () =>
+            Timing.CallDelayed(10, () =>
             {
-                for (int i = 0; i < 5; i++)
+                Owner.Role.Set(ev.TargetOldRole, RoleSpawnFlags.None);
+
+                Timing.CallDelayed(Timing.WaitForOneFrame, () =>
                 {
-                    try
+                    for (int i = 0; i < 5; i++)
                     {
-                        Owner.AddAbility(ABattle.Instance.GetRandomAbilities(ev.Player, ABattle.Instance.GetCategory(Owner), 1)[0]);
+                        try
+                        {
+                            Owner.AddAbility(ABattle.Instance.GetRandomAbilities(ev.Player,
+                                ABattle.Instance.GetCategory(Owner), 1)[0]);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error($"Failed to add ability to Mad Scientist: {ex}");
+                        }
                     }
-                    catch (Exception ex)
-                    {
-                        Log.Error($"Failed to add ability to Mad Scientist: {ex}");
-                    }
-                }
+                });
             });
         });
+    }
+
+    private void OnDetonating(DetonatingEventArgs e)
+    {
+        _isDetonatingState = true;
+
+        Timing.CallDelayed(Timing.WaitForOneFrame, () => _isDetonatingState = false);
     }
 }
--- RGM/RGM/Modes/Plus/ABattle/Abilities/Normal/Insurance.cs
+++ RGM/RGM/Modes/Plus/ABattle/Abilities/Normal/Insurance.cs
@@ -1,4 +1,5 @@
 ﻿using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Warhead;
 using MEC;
 using RGM.API.DataBases;
 using RGM.API.Features;
@@ -8,33 +9,44 @@ namespace RGM.Modes.Abilities.Normal;
 [Ability("보험", "사망 판정을 받을 경우 1번 버텨냅니다.", AbilityCategory.Common, AbilityType.NORMAL_INSURANCE)]
 public class Insurance : Ability
 {
+    private static bool _isDetonatingState;
+    
     public override void OnEnabled()
     {
         Exiled.Events.Handlers.Player.Dying += OnDying;
+        Exiled.Events.Handlers.Warhead.Detonating += OnDetonating;
     }
 
     public override void OnDisabled()
     {
         Exiled.Events.Handlers.Player.Dying -= OnDying;
+        Exiled.Events.Handlers.Warhead.Detonating -= OnDetonating;
     }
 
-    public void OnDying(DyingEventArgs ev)
+    private void OnDying(DyingEventArgs ev)
     {
-        if (ev.Player != Owner || ABattle.Instance.IsLifeUsed[Owner] || Datas.BlockDamageTypes.Contains(ev.DamageHandler.Type)) 
-            return;
-
-        ev.IsAllowed = false;
-        ev.Player.RemoveAbility(this);
-        OnDisabled();
-
-        Owner.AddAbility(AbilityType.DUMMY_EXPIREDINSURANCE);
-        Owner.AddHint("보험", $"사망 판정을 받았지만 <color={ABattle.RatingColor["일반"]}>보험</color>으로 인해 1번 버텨냅니다.");
-
-        ABattle.Instance.IsLifeUsed[Owner] = true;
-
         Timing.CallDelayed(Timing.WaitForOneFrame, () => 
         {
-            ABattle.Instance.IsLifeUsed[Owner] = false;
+            if (ev.Player != Owner || ABattle.Instance.IsLifeUsed[Owner] || Datas.BlockDamageTypes.Contains(ev.DamageHandler.Type) || _isDetonatingState) 
+                return;
+            
+            ev.IsAllowed = false;
+            ev.Player.RemoveAbility(this);
+            OnDisabled();
+
+            Owner.AddAbility(AbilityType.DUMMY_EXPIREDINSURANCE);
+            Owner.AddHint("보험", $"사망 판정을 받았지만 <color={ABattle.RatingColor["일반"]}>보험</color>으로 인해 1번 버텨냅니다.");
+
+            ABattle.Instance.IsLifeUsed[Owner] = true;
+            Timing.CallDelayed(Timing.WaitForOneFrame, () => ABattle.Instance.IsLifeUsed[Owner] = false);
         });
     }
+
+    private void OnDetonating(DetonatingEventArgs e)
+    {
+        if (_isDetonatingState) return;
+        
+        _isDetonatingState = true;
+        Timing.CallDelayed(Timing.WaitForOneFrame, () => _isDetonatingState = false);
+    } 
 }
