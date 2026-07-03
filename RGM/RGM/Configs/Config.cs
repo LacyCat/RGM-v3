1892cf97015264154e92cadc12396fb3f2598bf0
babycatoffical <wjsgusals32@gmail.com>
2026-07-03 22:20:50 +0900

    feat. Config.cs에 출력 언어 설정을 위한 EN 옵션의 설명 추가
    Signed-off-by: babycatoffical <wjsgusals32@gmail.com>

--- RGM/RGM/Configs/Config.cs
+++ RGM/RGM/Configs/Config.cs
@@ -8,6 +8,7 @@ namespace RGM
     {
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
+        [Description("이것이 참일 시, 출력 시스템은 영어로 동작할 것입니다. (If true, the output will be in English.)")]
         public bool EN { get; set; } = false;
         [Description("여기에 특정 모드의 Enum을 입력하면 그 모드가 고정적으로 나옵니다. 랜덤게임모드 관련 기능들이 일부 차단됩니다.")]
         public List<ModeType> FixedModes { get; set; } = new();
