9547980a1f16afa8634bedb0fc8b1d09b33ed140
Evan0717 <joungho0405@gmail.com>
2026-07-09 13:32:46 +0900

    전략적 팀 전투 | 증강 수정

--- RGM/RGM/Modes/Lock/TFT/TFT/TFTAbilities/Keter/All/AddDefense3.cs
+++ RGM/RGM/Modes/Lock/TFT/TFT/TFTAbilities/Keter/All/AddDefense3.cs
@@ -6,10 +6,11 @@ using static RGM.Variables.Variable;
 
 namespace DAONTFT.Core.TFT.Keter.All;
 
-[TFTAbility("방어 · 통달", "방어력을 20% 얻습니다. 추가로, 모든 받는 데미지가 최대 45까지만 적용됩니다. 능력 발동 시, 1.4초간 무적이 됩니다.", TFTAbilityLevel.Keter, TFTAbilityCategory.All, TFTAbilityPoint.Once, TFTAbilityType.AddDefense3, "⛔")]
+[TFTAbility("방어 · 통달", "방어력을 20% 얻습니다. 추가로, 모든 받는 데미지가 최대 55까지만 적용됩니다. 능력 발동 시, 1.4초간 무적이 됩니다.", TFTAbilityLevel.Keter, TFTAbilityCategory.All, TFTAbilityPoint.Once, TFTAbilityType.AddDefense3, "⛔")]
 public class AddDefense3 : TFTAbility
 {
-    const float MaxDamage = 45f;
+    const float MaxDamage = 55f;
+    const float GodModeDuration = 1.4f;
 
     public override void OnEnabled()
     {
@@ -32,26 +33,25 @@ public class AddDefense3 : TFTAbility
         {
             ev.IsAllowed = false;
             ev.Player.Hurt(MaxDamage, ev.DamageHandler.Type);
-            GodModePlayers.Add(Owner);
-
-            Timing.CallDelayed(1.4f, () =>
-            {
-                if (GodModePlayers.Contains(Owner))
-                    GodModePlayers.Remove(Owner);
-            });
-            return;
+            ActivateGodMode();
         }
 
         if (ev.DamageHandler.Damage > MaxDamage)
         {
             ev.DamageHandler.Damage = MaxDamage;
-            GodModePlayers.Add(Owner);
-
-            Timing.CallDelayed(1.4f, () =>
-            {
-                if (GodModePlayers.Contains(Owner))
-                    GodModePlayers.Remove(Owner);
-            });
+            ActivateGodMode();
         }
+    }
+    private void ActivateGodMode()
+    {
+        if (GodModePlayers.Contains(Owner))
+            return;
+
+        GodModePlayers.Add(Owner);
+        Timing.CallDelayed(GodModeDuration, () =>
+        {
+            if (GodModePlayers.Contains(Owner))
+                GodModePlayers.Remove(Owner);
+        });
     }
 }
